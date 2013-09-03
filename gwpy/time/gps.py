9a9bdf03843a94441bca7d893c1dc6ed05b190b1
Duncan Macleod <duncan.macleod@ligo.org>
2013-09-03 12:43:39 -0500

    time.gps: updated to include upcoming astropy GPS

--- gwpy/time/gps.py
+++ gwpy/time/gps.py
@@ -16,25 +16,25 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-"""Docstring
+"""Defines GPS time format, if astropy doesn't supply it
 """
-
-from astropy.time import *
-from astropy.time.core import SECS_PER_DAY
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 
+try:
+    from astropy.time import TimeGPS
+except ImportError:
+    from astropy.time.core import TimeFromEpoch,SECS_PER_DAY,TIME_FORMATS
 
-class TimeGPS(TimeFromEpoch):
-    """GPS time format: seconds from 1980-01-06 00:00:00 UTC.
-    """
-    name = 'gps'
-    precision = 9
-    unit = 1 / float(SECS_PER_DAY)
-    epoch_val = '1980-01-06 00:00:00'
-    epoch_val2 = None
-    epoch_scale = 'utc'
-    epoch_format = 'iso'
+    class TimeGPS(TimeFromEpoch):
+        """GPS time format: seconds from 1980-01-06 00:00:00 UTC.
+        """
+        name = 'gps'
+        precision = 9
+        unit = 1 / float(SECS_PER_DAY)
+        epoch_val = '1980-01-06 00:00:19'
+        epoch_val2 = None
+        epoch_scale = 'tai'
+        epoch_format = 'iso'
 
-
-TIME_FORMATS[TimeGPS.name] = TimeGPS
+    TIME_FORMATS[TimeGPS.name] = TimeGPS
