a54194cf740130c8d0ecdfaba20af36cf3d27002
Duncan Macleod <duncan.macleod@ligo.org>
2013-09-19 12:34:42 -0500

    gwpy.window: new module to window in time-domain
        - thin wrapper around scipy.signal windows
        - currently a memory alloc error in the to_lal function, bumped to
          LIGO DASWG

---
+++ gwpy/window/__init__.py
@@ -0,0 +1,12 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""Create and apply time-domain or frequency-domain windows to Series
+"""
+
+from .. import version
+__author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
+__version__ = version.version
+
+from .core import *
+
+        
