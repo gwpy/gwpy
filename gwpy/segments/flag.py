9baf49d3a8aa09295bfafbdc33ff5dce089c89a4
Duncan Macleod <duncan.macleod@ligo.org>
2013-10-02 17:25:49 -0500

    DataQualityFlag: multiple new features
        - add type class attributes to control element setting
        - added a whole bunch of new functions, including prototypes for
          mapping to LIGO_LW tables
        - added prototype DataQualityList class for grouping flags

--- gwpy/segments/flag.py
+++ gwpy/segments/flag.py
@@ -5,11 +5,11 @@ named set of segments used in defining GW instrument state or
 data quality information.
 """
 
-import math
 import operator
 import numpy
 import re
 import warnings
+from math import (floor, ceil)
 
 from astropy.io import registry as io_registry
 
@@ -29,7 +29,12 @@ class DataQualityFlag(object):
         - '`valid`' segments: times during which this flag was well defined
         - '`active`' segments: times during which this flag was active
     """
-    def __init__(self, name=None, active=[], valid=[]):
+    EntryClass = Segment
+    ListClass = SegmentList
+    __slots__ = ('_active', '_valid', 'ifo', 'name', 'version',
+                 'category', 'comment')
+    def __init__(self, name=None, active=[], valid=[], category=None,
+                 comment=None):
         """Define a new DataQualityFlag, with a name, a set of active
         segments, and a set of valid segments
 
@@ -47,6 +52,8 @@ class DataQualityFlag(object):
         self.ifo, self.name, self.version = parse_flag_name(name)
         self.valid = valid
         self.active = active
+        self.category = category
+        self.comment = comment
 
     @property
     def active(self):
@@ -59,7 +66,8 @@ class DataQualityFlag(object):
             return self._active
     @active.setter
     def active(self, segmentlist):
-        self._active = SegmentList(map(Segment, segmentlist)).coalesce()
+        self._active = self.ListClass(map(self.EntryClass,
+                                          segmentlist)).coalesce()
         if not self.valid and len(segmentlist):
             self.valid = [segmentlist.extent()]
 
@@ -71,11 +79,24 @@ class DataQualityFlag(object):
         return self._valid
     @valid.setter
     def valid(self, segmentlist):
-        self._valid = SegmentList(map(Segment, segmentlist)).coalesce()
+        self._valid = self.ListClass(map(self.EntryClass,
+                                         segmentlist)).coalesce()
 
     def efficiency(self, trigtable):
         raise NotImplementedError("Class-level efficiency calculation has "
                                   "not been implemented yet.")
+
+    def __getitem__(self, item):
+        if isinstance(item, int) or isinstance(item, slice):
+            return self.active[item]
+        else:
+            new = self.__class__()
+            new.ifo = self.ifo
+            new.name = self.name
+            new.version = self.version
+            new.active = self.active[item]
+            new.valid = self.valid
+            return new
 
     @classmethod
     def query(cls, flag, gpsstart, gpsend,
@@ -107,7 +128,7 @@ class DataQualityFlag(object):
         if not version:
             version = '*'
         gpsstart = int(float(gpsstart))
-        gpsend = int(math.ceil(float(gpsend)))
+        gpsend = int(ceil(float(gpsend)))
         connection = segdb_utils.setup_database(url)
         engine = segdb_engine.LdbdQueryEngine(connection)
 
@@ -121,6 +142,104 @@ class DataQualityFlag(object):
                    active=reduce(operator.or_, segs).coalesce())
 
     read = classmethod(io_registry.read)
+
+    def round(self):
+        """Expand this `DataQualityFlag` to integer segments, with each
+        segment protracted to the nearest integer boundaries
+        """
+        new = self.copy()
+        new.active = self.ListClass([self.EntryClass(int(floor(s[0])),
+                                                     int(ceil(s[1]))) for
+                                     s in new.active])
+        new.valid = self.ListClass([self.EntryClass(int(floor(s[0])),
+                                                    int(ceil(s[1]))) for
+                                    s in new.valid])
+        return new.coalesce()
+
+    def coalesce(self):
+        """Run the coalesce method over both the active and valid
+        SegmentLists
+        """
+        self.active = self.active.coalesce()
+        self.valid = self.valid.coalesce()
+        return self
+
+    def to_ligolw(self):
+        """Return a formatted LIGO_LW document describing the full
+        content of this `DataQualityFlag`.
+
+        Returns
+        -------
+        xmldoc : :class`~glue.ligolw.ligolw.Document`
+            a formatted LIGO_LW XML document with the following tables:
+
+                - :class:`~glue.ligolw.lsctables.VetoDefTable` -
+                  defining each of the valid segments
+                - :class:`~glue.ligolw.lsctables.SegmentDefTable` -
+                  defining the segments for this flag
+                - :class:`~glue.ligolw.lsctables.SegmentSumTable` -
+                  summarising the valid segments, this should be a
+                  one-to-one mapping with the `VetoDefTable`
+                - :class:`~glue.ligolw.lsctables.SegmentTable` -
+                  listing each of the active segments
+        """
+        raise NotImplementedError()
+
+    def to_ligolw_veto_definer_table(self):
+        """Return a formatted LIGO_LW veto-definer table describing
+        the valid segments for this `DataQualityFlag`
+
+        Returns
+        -------
+        lsctable : :class:`~glue.ligolw.lsctable.VetoDefTable`
+            XML table defining each of the valid segments for this flag
+        """
+        raise NotImplementedError()
+
+
+    def to_ligolw_segment_table(self):
+        """Return a formatted LIGO_LW segment table describing
+        the active segments for this `DataQualityFlag`
+
+        Returns
+        -------
+        lsctable : :class:`~glue.ligolw.lsctable.SegmentTable`
+            XML table listing the active segments for this flag
+        """
+        raise NotImplementedError()
+
+    def __repr__(self):
+        indent = " " * len("<%s(" % self.__class__.__name__)
+        valid = str(self.valid).replace("\n",
+                                         "\n%s      " % indent)
+        active = str(self.active).replace("\n",
+                                           "\n%s       " % indent)
+        return ("<{1}(valid={2},\n{0}active={3},\n{0}ifo={4},\n{0}name={5},\n"
+                "{0}version={6},\n{0}comment={7})>".format(
+                    indent, self.__class__.__name__, valid, active,
+                    repr(self.ifo), repr(self.name), repr(self.version),
+                    repr(self.comment)))
+
+    def copy(self):
+        new = self.__class__()
+        new.ifo = self.ifo
+        new.name = self.name
+        new.version = self.version
+        new.comment = self.comment
+        new.valid = self.ListClass([self.EntryClass(s[0], s[1]) for
+                                    s in self.valid])
+        new.active = self.ListClass([self.EntryClass(s[0], s[1]) for
+                                     s in self.active])
+        return new
+
+class DataQualityList(list):
+    EntryClass = DataQualityFlag
+    def __init__(self, *entries):
+        list.__init__(*entries)
+
+    def to_ligolw(self):
+        raise NotImplementedError()
+
 
 _re_inv = re.compile(r"\A(?P<ifo>[A-Z]\d):(?P<name>[^/]+):(?P<version>\d+)\Z")
 _re_in = re.compile(r"\A(?P<ifo>[A-Z]\d):(?P<name>[^/]+)\Z")
