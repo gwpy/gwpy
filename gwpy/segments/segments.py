4608bae4da6eeb1620564176dc75a372c699e78e
Duncan Macleod <duncan.macleod@ligo.org>
2013-10-02 17:25:30 -0500

    segments: changed sub-classing of glue segments

--- gwpy/segments/segments.py
+++ gwpy/segments/segments.py
@@ -6,12 +6,14 @@ gravitational-wave laser interferometer was operating in a specific
 configuration.
 """
 
+from math import (ceil, floor)
+
 from ..version import version as __version__
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 __credits__ = "Kipp Cannon <kipp.cannon@ligo.org>"
 
 from glue.segments import (segment as _Segment,
-                           segmentlist as SegmentList,
+                           segmentlist as _SegmentList,
                            segmentlistdict as SegmentListDict)
 
 
@@ -28,6 +30,23 @@ class Segment(_Segment):
         return self[1]
 
     def __repr__(self):
-        return "<%s(%s, %s)>" % (self.__class__.__name__, self[0], self[1])
+        return "%s(%s, %s)" % (self.__class__.__name__, self[0], self[1])
+
+    def __str__(self):
+        return "[%s ... %s)" % (self[0], self[1])
+
+class SegmentList(_SegmentList):
+    def __repr__(self):
+        return "<SegmentList([%s])>" % "\n              ".join(map(repr, self))
+
+    def coalesce(self):
+        self = super(SegmentList, self).coalesce()
+        self = self.__class__([Segment(s[0], s[1]) for s in self])
+        return self
+    coalesce.__doc__ = _SegmentList.coalesce.__doc__
+
+    def __str__(self):
+        return "[%s]" % "\n ".join(map(str, self))
 
 del _Segment
+del _SegmentList
