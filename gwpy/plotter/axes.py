36aa88a8871121d341624fda928b0921ecd03e37
Duncan Macleod <duncan.macleod@ligo.org>
2013-10-03 12:18:45 -0500

    plotter: new classes for axes-centric plotting
        - plotter.fig: defines simple extension of mpl Figure
        - plott.axes: defines simple extension of mpl Axes

---
+++ gwpy/plotter/axes.py
@@ -0,0 +1,132 @@
+
+"""Extension of the :class:`~matplotlib.axes.Axes` class with
+user-friendly attributes
+"""
+
+from matplotlib.axes import Axes as _Axes
+from .decorators import auto_refresh
+
+class Axes(_Axes):
+    # -----------------------------------------------
+    # text properties
+
+    # x-axis label
+    @property
+    def xlabel(self):
+        """Label for the x-axis
+
+        :type: :class:`~matplotlib.text.Text`
+        """
+        return self.xaxis.label
+    @xlabel.setter
+    @auto_refresh
+    def xlabel(self, text):
+        if isinstance(text, basestring):
+            self.set_xlabel(text)
+        else:
+            self.xaxis.label = text
+    @xlabel.deleter
+    @auto_refresh
+    def xlabel(self):
+        self.set_xlabel("")
+
+    # y-axis label
+    @property
+    def ylabel(self):
+        """Label for the y-axis
+
+        :type: :class:`~matplotlib.text.Text`
+        """
+        return self.yaxis.label
+    @ylabel.setter
+    @auto_refresh
+    def ylabel(self, text):
+        if isinstance(text, basestring):
+            self.set_ylabel(text)
+        else:
+            self.yaxis.label = text
+    @ylabel.deleter
+    @auto_refresh
+    def ylabel(self):
+        self.set_ylabel("")
+
+    # -----------------------------------------------
+    # limit properties
+
+    @property
+    def xlim(self):
+        """Limits for the x-axis
+
+        :type: `tuple`
+        """
+        return self.get_xlim()
+    @xlim.setter
+    @auto_refresh
+    def xlim(self, limits):
+        self.set_xlim(*limits)
+    @xlim.deleter
+    @auto_refresh
+    def xlim(self):
+        self.relim()
+        self.autoscale_view(scalex=True, scaley=False)
+
+    @property
+    def ylim(self):
+        """Limits for the y-axis
+
+        :type: `tuple`
+        """
+        return self.get_ylim()
+    @ylim.setter
+    @auto_refresh
+    def ylim(self, limits):
+        self.set_ylim(*limits)
+    @ylim.deleter
+    def ylim(self):
+        self.relim()
+        self.autoscale_view(scalex=False, scaley=True)
+
+    # -----------------------------------------------
+    # scale properties
+
+    @property
+    def logx(self):
+        """Display the x-axis with a logarithmic scale
+
+        :type: `bool`
+        """
+        return self.axes.get_xscale() == "log"
+    @logx.setter
+    @auto_refresh
+    def logx(self, log):
+        self.axes.set_xscale(log and "log" or "linear")
+
+    @property
+    def logy(self):
+        """Display the y-axis with a logarithmic scale
+
+        :type: `bool`
+        """
+        return self.axes.get_yscale() == "log"
+    @logy.setter
+    @auto_refresh
+    def logy(self, log):
+        self.axes.set_yscale(log and "log" or "linear")
+
+    # -------------------------------------------
+    # Axes methods
+
+    @auto_refresh
+    def resize(self, pos, which='both'):
+        """Set the axes position with::
+
+            pos = [left, bottom, width, height]
+
+        in relative 0,1 coords, or *pos* can be a
+        :class:`~matplotlib.transforms.Bbox`
+
+        There are two position variables: one which is ultimately
+        used, but which may be modified by :meth:`apply_aspect`, and a
+        second which is the starting point for :meth:`apply_aspect`.
+        """
+        return super(Axes, self).set_position(pos, which='both')
---
+++ gwpy/plotter/fig.py
@@ -0,0 +1,76 @@
+
+"""Extension of the basic matplotlib Figure for GWpy
+"""
+
+import inspect
+
+from matplotlib import (figure, pyplot)
+
+from . import IS_INTERACTIVE
+
+
+class Plot(figure.Figure):
+    """An extension of the matplotib :class:`~matplotlib.figure.Figure`
+    object for GWpy
+    """
+    def __init__(self, auto_refresh=IS_INTERACTIVE, **kwargs):
+        # call pyplot.figure to get a better object, but protect against
+        # recursion
+        super(Plot, self).__init__(**kwargs)
+        self._auto_refresh = auto_refresh
+
+    def add_timeseries(self, timeseries, ax=None, **kwargs):
+        """Add a :class:`~gwpy.timeseries.core.TimeSeries` trace to this plot
+
+        Parameters
+        ----------
+        timeseries : :class:`~gwpy.timeseries.core.TimeSeries`
+            the TimeSeries to display
+        **kwargs
+            other keyword arguments for the `Plot.add_line` function
+
+        Returns
+        -------
+        Line2D
+            the :class:`~matplotlib.lines.Line2D` for this line layer
+        """
+        # find relevant axes
+        if ax is None and self.axes:
+            from .timeseries import TimeSeriesAxes
+            tsaxes = [a for a in self.axes if isinstance(a, TimeSeriesAxes)]
+            if tsaxes:
+                ax = tsaxes[0]
+        if ax is None:
+            ax = self.add_subplot(111, projection='timeseries')
+        # plot on axes
+        kwargs.setdefault('label', timeseries.name)
+        return ax.plot(timeseries, **kwargs)
+
+    # -----------------------------------------------
+    # core plot operations
+
+    def refresh(self):
+        """Refresh the current figure
+        """
+        self.canvas.draw()
+
+    def show(self):
+        """Display the current figure
+        """
+        self.patch.set_alpha(0.0)
+        super(Plot, self).show()
+
+    def save(self, *args, **kwargs):
+        """Save the figure to disk.
+
+        All `args` and `kwargs` are passed directly to the savefig
+        method of the underlying `matplotlib.figure.Figure`
+        self.fig.savefig(*args, **kwargs)
+        """
+        self.savefig(*args, **kwargs)
+
+    def close(self):
+        """Close the plot and release its memory.
+        """
+        mpl.close(self)
+
