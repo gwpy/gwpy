aac5ad7cf2d7b1a60f0a7c6478a6fa71741686c0
Duncan Macleod <duncan.macleod@ligo.org>
2013-10-18 10:45:18 -0500

    LICENSE: added GNU v3 license to all files

--- gwpy/__init__.py
+++ gwpy/__init__.py
@@ -1,4 +1,20 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# Copyright (C) Duncan Macleod (2013)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
 
 """Package to do gravitational wave astrophysics with python
 """
@@ -6,7 +22,7 @@
 # import core astropy modules
 from astropy import table
 try:
-    from astropy.units.quantity import WARN_IMPLICIT_NUMERIC_CONVERSION
+# from astropy.units.quantity import WARN_IMPLICIT_NUMERIC_CONVERSION
 except ImportError:
     pass
 else:
