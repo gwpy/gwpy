e0efb542d4da095e1b9b688db01afe2cdeeb7104
Duncan Macleod <duncan.macleod@ligo.org>
2013-10-18 16:34:05 -0500

    Minor edit to auto_refresh

--- gwpy/plotter/decorators.py
+++ gwpy/plotter/decorators.py
@@ -43,10 +43,10 @@ def auto_refresh(f, *args, **kwargs):
         mydata.nesting -= 1
         if hasattr(args[0], 'figure') and args[0].figure is not None:
             if refresh and mydata.nesting == 0 and args[0].figure._auto_refresh:
-                args[0].figure.canvas.draw()
+                args[0].figure.refresh()
         elif isinstance(args[0], Figure):
             if refresh and mydata.nesting == 0 and args[0]._auto_refresh:
-                args[0].canvas.draw()
+                args[0].refresh()
 
 @decorator
 def axes_method(f, *args, **kwargs):
