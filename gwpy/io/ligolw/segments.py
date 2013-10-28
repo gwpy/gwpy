c4cf66e79ba39bc07d6b4721318b03e2ab1bd3a2
Duncan Macleod <duncan.macleod@ligo.org>
2013-10-28 17:49:40 -0500

    io.ligolw.segments: minor docstring change
        - TODO: this module needs better documentation

--- gwpy/io/ligolw/segments.py
+++ gwpy/io/ligolw/segments.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Read segment XML into DataQualityFlags
+"""Read/write segment XML in LIGO_LW format into DataQualityFlags
 """
 
 import re
