1a287eb8a4685204f4b5180320b604a794c28ae6
Michael Coughlin <michael.coughlin@ligo.org>
2013-11-17 07:16:06 -0800

    Add the decimate function to series.py so older versions can use it.

--- gwpy/data/series.py
+++ gwpy/data/series.py
@@ -219,3 +219,55 @@ class Series(Array):
         return Quantity(super(Series, self).median(*args, **kwargs),
                         unit=self.unit)
     median.__doc__ = Array.median.__doc__
+
+def decimate(x, q, n=None, ftype='iir', axis=-1):
+    """
+    Downsample the signal by using a filter.
+
+    By default, an order 8 Chebyshev type I filter is used. A 30 point FIR
+    filter with hamming window is used if `ftype` is 'fir'.
+
+    Parameters
+    ----------
+    x : ndarray
+    The signal to be downsampled, as an N-dimensional array.
+    q : int
+    The downsampling factor.
+    n : int, optional
+    The order of the filter (1 less than the length for 'fir').
+    ftype : str {'iir', 'fir'}, optional
+    The type of the lowpass filter.
+    axis : int, optional
+    The axis along which to decimate.
+
+    Returns
+    -------
+    y : ndarray
+    The down-sampled signal.
+
+    See also
+    --------
+    resample
+
+    """
+
+    if not isinstance(q, int):
+        raise TypeError("q must be an integer")
+
+    if n is None:
+        if ftype == 'fir':
+            n = 30
+        else:
+            n = 8
+
+    if ftype == 'fir':
+        b = signal.firwin(n + 1, 1. / q, window='hamming')
+        a = 1.
+    else:
+        b, a = signal.cheby1(n, 0.05, 0.8 / q)
+
+    y = signal.lfilter(b, a, x, axis=axis)
+
+    sl = [slice(None)] * y.ndim
+    sl[axis] = slice(None, None, q)
+    return y[sl]
