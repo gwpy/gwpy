8d5a85301868bedf657203cd5973bb4b832c1268
Duncan Macleod <duncan.macleod@ligo.org>
2014-01-30 15:22:24 -0600

    psd.lal_psd: added XLAL plan and window creation
        - all auto-generated plans and windows are cached

--- gwpy/spectrum/psd.py
+++ gwpy/spectrum/psd.py
@@ -36,8 +36,7 @@ from scipy import signal
 from astropy import units
 
 from .core import Spectrum
-from ..timeseries import window as tdwindow
-from ..spectrogram import Spectrogram
+from ..window import Window
 
 from .. import version
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
@@ -46,12 +45,12 @@ __version__ = version.version
 __all__ = ['bartlett', 'welch', 'median_mean', 'median', 'spectrogram']
 
 # cache windows internally
-LALWINDOWS = {}
+LAL_WINDOWS = {}
 LAL_FFTPLANS = {}
-LAL_FFTPLAN_LEVEL = 1
+LAL_FFTPLAN_LEVEL = 3
 
 
-def bartlett(timeseries, segmentlength, window=None):
+def bartlett(timeseries, segmentlength, window=None, plan=None):
     """Calculate the power spectral density of the given `TimeSeries`
     using the Bartlett average method.
 
@@ -64,16 +63,19 @@ def bartlett(timeseries, segmentlength, window=None):
         input `TimeSeries` data
     segmentlength : `int`
         number of samples in each average
+    plan : :lalsuite:`XLALREAL8ForwardFFTPlan`, optional
+        LAL FFT plan to use when generating average spectrum
 
     Returns
     -------
     Spectrum
         Bartlett-averaged `Spectrum`
     """
-    return welch(timeseries, segmentlength, segmentlength, window=window)
+    return welch(timeseries, segmentlength, segmentlength, window=window,
+                 plan=None)
 
 
-def welch(timeseries, segmentlength, overlap, window=None):
+def welch(timeseries, segmentlength, overlap, window=None, plan=None):
     """Calculate the power spectral density of the given `TimeSeries`
     using the Welch average method.
 
@@ -89,8 +91,10 @@ def welch(timeseries, segmentlength, overlap, window=None):
         number of samples in single average
     overlap : `int`
         number of samples between averages
-    window : `timeseries.Window`, optional
+    window : `~gwpy.window.Window`, optional
         window function to apply to timeseries prior to FFT
+    plan : :lalsuite:`XLALREAL8ForwardFFTPlan`, optional
+        LAL FFT plan to use when generating average spectrum
 
     Returns
     -------
@@ -99,13 +103,13 @@ def welch(timeseries, segmentlength, overlap, window=None):
     """
     try:
         return lal_psd(timeseries, 'welch', segmentlength, overlap,
-                       window=window)
+                       window=window, plan=None)
     except ImportError:
         return scipy_psd(timeseries, 'welch', segmentlength, overlap,
                          window=window)
 
 
-def median_mean(timeseries, segmentlength, overlap, window=None):
+def median_mean(timeseries, segmentlength, overlap, window=None, plan=None):
     """Calculate the power spectral density of the given `TimeSeries`
     using the median-mean average method.
 
@@ -119,8 +123,10 @@ def median_mean(timeseries, segmentlength, overlap, window=None):
         number of samples in single average
     overlap : `int`
         number of samples between averages
-    window : `timeseries.Window`, optional
+    window : `~gwpy.window.Window`, optional
         window function to apply to timeseries prior to FFT
+    plan : :lalsuite:`XLALREAL8ForwardFFTPlan`, optional
+        LAL FFT plan to use when generating average spectrum
 
     Returns
     -------
@@ -128,10 +134,10 @@ def median_mean(timeseries, segmentlength, overlap, window=None):
         median-mean-averaged `Spectrum`
     """
     return lal_psd(timeseries, 'medianmean', segmentlength, overlap,
-                    window=window)
+                    window=window, plan=None)
 
 
-def median(timeseries, segmentlength, overlap, window=None):
+def median(timeseries, segmentlength, overlap, window=None, plan=None):
     """Calculate the power spectral density of the given `TimeSeries`
     using the median-mean average method.
 
@@ -145,8 +151,10 @@ def median(timeseries, segmentlength, overlap, window=None):
         number of samples in single average
     overlap : `int`
         number of samples between averages
-    window : `timeseries.Window`, optional
+    window : `~gwpy.window.Window`, optional
         window function to apply to timeseries prior to FFT
+    plan : :lalsuite:`XLALREAL8ForwardFFTPlan`, optional
+        LAL FFT plan to use when generating average spectrum
 
     Returns
     -------
@@ -154,10 +162,10 @@ def median(timeseries, segmentlength, overlap, window=None):
         median-mean-averaged `Spectrum`
     """
     return lal_psd(timeseries, 'medianmean', segmentlength, overlap,
-                    window=window)
+                    window=window, plan=None)
 
 
-def lal_psd(timeseries, method, segmentlength, overlap, window=None):
+def lal_psd(timeseries, method, segmentlength, overlap, window=None, plan=None):
     """Internal wrapper to the `lal.spectrum.psd` function
 
     This function handles the conversion between GWpy `TimeSeries` and
@@ -173,8 +181,10 @@ def lal_psd(timeseries, method, segmentlength, overlap, window=None):
         number of samples in single average
     overlap : `int`
         number of samples between averages
-    window : `timeseries.Window`, optional
+    window : `~gwpy.window.Window`, optional
         window function to apply to timeseries prior to FFT
+    plan : :lalsuite:`XLALREAL8ForwardFFTPlan`, optional
+        LAL FFT plan to use when generating average spectrum
 
     Returns
     -------
@@ -196,22 +206,16 @@ def lal_psd(timeseries, method, segmentlength, overlap, window=None):
     stype = lalutils.dtype(lalts)
     # get cached window
     if window is None:
-        global LALWINDOWS
-        if segmentlength not in LALWINDOWS:
-            LALWINDOWS[segmentlength] = getattr(
-                        lal, 'CreateKaiser%sWindow' % stype)(segmentlength, 24)
-        lalwin = LALWINDOWS[segmentlength]
-    else:
-        lalwin = window.to_lal()
+        window = generate_lal_window(segmentlength, dtype=timeseries.dtype)
+    elif isinstance(window, Window):
+        window = window.to_lal()
     # get cached FFT plan
-    global LAL_FFTPLANS
-    if segmentlength not in LAL_FFTPLANS:
-        LAL_FFTPLANS[segmentlength] = getattr(
-                       lal, 'CreateForward%sFFTPlan' % stype)(segmentlength,
-                                                              LAL_FFTPLAN_LEVEL)
-    fftplan = LAL_FFTPLANS[segmentlength]
+    if plan is None:
+        plan = generate_lal_fft_plan(segmentlength, dtype=timeseries.dtype)
+    # generate average spectrum
     lalfs = lalspectrum._psd(method, lalts, segmentlength, overlap,
-                             window=lalwin, plan=fftplan)
+                             window=window, plan=plan)
+    # format and return
     spec = Spectrum.from_lal(lalfs)
     spec.channel = timeseries.channel
     if timeseries.unit:
@@ -220,7 +224,8 @@ def lal_psd(timeseries, method, segmentlength, overlap, window=None):
         spec.unit = 1 / units.Hertz
     return spec
 
-def scipy_psd(timeseries, method, segmentlength, overlap, window='hanning'):
+
+def scipy_psd(timeseries, method, segmentlength, overlap, window='hamming'):
     """Internal wrapper to the `lal.spectrum.psd` function
 
     This function handles the conversion between GWpy `TimeSeries` and
@@ -236,7 +241,7 @@ def scipy_psd(timeseries, method, segmentlength, overlap, window='hanning'):
         number of samples in single average
     overlap : `int`
         number of samples between averages
-    window : `timeseries.Window`, optional
+    window : `~gwpy.window.Window`, optional
         window function to apply to timeseries prior to FFT
 
     Returns
@@ -265,3 +270,77 @@ def scipy_psd(timeseries, method, segmentlength, overlap, window='hanning'):
     else:
         spec.unit = 1 / units.Hertz
     return spec
+
+
+def generate_lal_fft_plan(length, level=LAL_FFTPLAN_LEVEL,
+                          dtype=numpy.dtype(numpy.float64)):
+    """Build a plan to use when performing a Fourier transform using
+    the LIGO Algorithm Library routines.
+
+    Parameters
+    ----------
+    length : `int`
+        number of samples to plan for in each FFT.
+    level : `int`, optional
+        amount of work to do when planning the FFT, default set by
+        LAL_FFTPLAN_LEVEL module variable.
+    dtype : :class:`numpy.dtype`
+        numeric type of data to plan for, default `numpy.dtype(numpy.float64)`
+
+    Returns
+    -------
+    :lalsuite:`XLALREAL8ForwardFFTPlan`
+        FFT plan of the relevant data type
+    """
+    from lal import (lal, utils as lalutils)
+    global LAL_FFTPLANS
+    ltype = lalutils.LAL_TYPE_FROM_NUMPY[dtype.type]
+    laltypestr = lalutils.LAL_TYPE_STR[ltype]
+    try:
+        plan = LAL_FFTPLANS[(length, laltypestr)]
+    except KeyError:
+        create = getattr(lal, 'CreateForward%sFFTPlan' % laltypestr)
+        plan = LAL_FFTPLANS[(length, laltypestr)] = create(length, level)
+    return plan
+
+
+def generate_lal_window(length, type='hamming',
+                        dtype=numpy.dtype(numpy.float64)):
+    """Generate a time-domain window for use in a Fourier transform using
+    the LIGO Algorithm Library routines.
+
+    Parameters
+    ----------
+    length : `int`
+        length of window in samples.
+    type : `str`, `tuple`
+        name of window to generate, default: ``'hamming'``. Give tuple of
+        ``(name, *args)`` for complicated windows, e.g. ``('kaiser', 24)``
+    dtype : :class:`numpy.dtype`
+        numeric type of window, default `numpy.dtype(numpy.float64)`
+
+    Returns
+    -------
+    `window` : XLAL window
+        time-domain window to use for FFT
+    """
+    from lal import (lal, utils as lalutils)
+    global LAL_WINDOWS
+    ltype = lalutils.LAL_TYPE_FROM_NUMPY[dtype.type]
+    laltypestr = lalutils.LAL_TYPE_STR[ltype]
+    wtype = isinstance(type, (list, tuple)) and type[0] or str(type)
+    try:
+        window = LAL_WINDOWS[(length, wtype.lower(), laltypestr)]
+    except KeyError:
+        if isinstance(type, (list, tuple)):
+            try:
+                args = type[1:]
+            except IndexError:
+                args = []
+        else:
+            args = []
+        wtype = wtype.islower() and wtype.title() or wtype
+        create = getattr(lal, 'Create%s%sWindow' % (wtype, laltypestr))
+        window = LAL_WINDOWS[(length, wtype.lower(), laltypestr)] = create(
+                                                                 length, *args)
+    return window
