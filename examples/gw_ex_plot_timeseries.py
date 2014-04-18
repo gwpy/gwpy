8199888cbf3dc275212cadd7622c0970bfa7a05f
Duncan Macleod <duncan.macleod@ligo.org>
2014-04-17 22:58:32 -0500

    examples: minor edits

--- examples/gw_ex_plot_timeseries.py
+++ examples/gw_ex_plot_timeseries.py
@@ -25,7 +25,7 @@ Problem
 I would like to study the gravitational wave strain time-series around the time of an interesting simulated signal during the last science run (S6). I have access to the frame files on the LIGO Data Grid machine `ldas-pcdev2.ligo-wa.caltech.edu` and so can read them directly.
 """
 
-from gwpy.time import Time
+from gwpy.time import gps
 from gwpy.timeseries import TimeSeries
 
 from gwpy import version
@@ -34,8 +34,8 @@ __version__ = version.version
 
 
 # set the times
-start = Time('2010-09-16 06:42:00', format='iso', scale='utc')
-end = Time('2010-09-16 06:43:00', format='iso', scale='utc')
+start = gps('2010-09-16 06:42:00')
+end = gps('2010-09-16 06:43:00')
 
 # make timeseries
 data = TimeSeries.fetch('H1:LDAS-STRAIN', start, end)
