679d00ceaf43e86884bf77ce3df0bd235c3df74e
Duncan Macleod <duncan.macleod@ligo.org>
2014-04-17 22:54:50 -0500

    docs: minor edits to plot examples

--- docs/spectrogram/spectrogram_plot.py
+++ docs/spectrogram/spectrogram_plot.py
@@ -1,9 +1,10 @@
 from gwpy.timeseries import TimeSeries
-gwdata = TimeSeries.fetch('H1:LDAS-STRAIN', 'September 16 2010 06:40', 'September 16 2010 06:50')
+gwdata = TimeSeries.fetch('H1:LDAS-STRAIN', 'September 16 2010 06:40',
+                          'September 16 2010 06:50')
 specgram = gwdata.spectrogram(20, fftlength=8, fftstride=4) ** (1/2.)
 plot = specgram.plot(norm='log', vmin=1e-23, vmax=1e-19)
 ax = plot.gca()
 ax.set_ylim(40, 4000)
 ax.set_yscale('log')
-plot.add_colorbar()
+plot.add_colorbar(label='Gravitational-wave strain [m/$\sqrt{\mathrm{Hz}}$')
 plot.show()
