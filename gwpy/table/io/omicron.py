935dae354c1f397de7720b0e86736d15b1e67f38
Duncan Macleod <duncan.macleod@ligo.org>
2014-05-02 18:13:25 -0500

    table.io.omicron: fix benign typo

--- gwpy/table/io/omicron.py
+++ gwpy/table/io/omicron.py
@@ -33,10 +33,10 @@ try:
 except ImportError:
     from glue.lal import LIGOTimeGPS
     seconds = lambda t: t.seconds
-    nanoeseconds = lambda t: t.nanoseconds
+    nanoseconds = lambda t: t.nanoseconds
 else:
     seconds = lambda t: t.gpsSeconds
-    nanoeseconds = lambda t: t.gpsNanoSeconds
+    nanoseconds = lambda t: t.gpsNanoSeconds
 
 from ROOT import TChain
 
@@ -92,17 +92,17 @@ def sngl_burst_from_root(tchain, columns=OMICRON_COLUMNS):
     if 'time' in columns or 'peak_time' in columns:
         t.peak_time = seconds(peak_time)
     if 'time' in columns or 'peak_time_ns' in columns:
-        t.peak_time_ns = nanoeseconds(peak_time)
+        t.peak_time_ns = nanoseconds(peak_time)
     start_time = LIGOTimeGPS(tchain.tstart)
     if 'start_time' in columns:
         t.start_time = seconds(start_time)
     if 'start_time_ns' in columns:
-        t.start_time_ns = nanoeseconds(start_time)
+        t.start_time_ns = nanoseconds(start_time)
     stop_time = LIGOTimeGPS(tchain.tend)
     if 'stop_time' in columns:
         t.stop_time = seconds(stop_time)
     if 'stop_time_ns' in columns:
-        t.stop_time_ns = nanoeseconds(stop_time)
+        t.stop_time_ns = nanoseconds(stop_time)
     if 'duration' in columns:
         t.duration = float(stop_time - start_time)
 
