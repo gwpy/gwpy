a8b4896addd98a6501a0ea058a79f14da642d866
Duncan Macleod <duncan.macleod@ligo.org>
2014-05-14 17:33:11 -0500

    docs/timeseries: use released data for TS

--- docs/timeseries/timeseries_plot.py
+++ docs/timeseries/timeseries_plot.py
@@ -1,6 +1,5 @@
 from gwpy.timeseries import TimeSeries
-gwdata = TimeSeries.fetch('H1:LDAS-STRAIN', 'September 16 2010 06:40',
-                          'September 16 2010 06:50')
+gwdata = TimeSeries.fetch('H1:LDAS-STRAIN', 968654552, 968654562)
 plot = gwdata.plot()
 ax = plot.gca()
 ax.set_ylabel('Gravitational-wave strain amplitude')
