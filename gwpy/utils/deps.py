95c2be39fe2ef072cd5635deb21c1468b9303087
Duncan Macleod <duncan.macleod@ligo.org>
2014-05-19 21:14:07 -0500

    utils.deps: improved import and added decorator

--- gwpy/utils/deps.py
+++ gwpy/utils/deps.py
@@ -16,25 +16,25 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
-"""This target provides a few utilities for handling optional
+"""This module provides a few utilities for handling optional
 dependencies within GWpy code.
 """
 
+from importlib import import_module
 import inspect
 
 from .. import version
-__author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
+__author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 __version__ = version.version
 
 
-def import_method_dependency(target):
-    """Import the given target, with a more useful `ImportError` message.
+def import_method_dependency(module, stacklevel=1):
+    """Import the given module, with a more useful `ImportError` message.
 
     Parameters
     ----------
-    target : `str`
-        name of the target object (module, method, class, or variable)
-        to import
+    module : `str`
+        name of the module to import
 
     Returns
     -------
@@ -47,8 +47,36 @@ def import_method_dependency(target):
         if the given object cannot be imported
     """
     try:
-        return __import__(target, fromlist=[''])
-    except ImportError:
-        caller = inspect.stack()[1][3]
-        raise ImportError("Cannot import %r required by the %s() method."
-                          % (target, caller))
+        return import_module(module)
+    except ImportError as e:
+        # get method name
+        caller = inspect.stack()[stacklevel][3]
+        # raise a better exception
+        if not e.args: 
+           e.args=('',)
+        e.args = ("Cannot import %s required by the %s() method: %r"
+                  % (module, caller, str(e)),) + e.args[1:]
+        raise
+
+
+def with_import(module):
+    """Decorate a given method to import an optional dependency.
+
+    Parameters
+    ----------
+    module : `str`
+        name of the module object to import
+
+    Returns
+    -------
+    decorator : `function`
+        a decorator to apply to a method with the optional import
+    """
+    modname = module.split('.')[-1]
+    def decorate_method(func):
+        def wrapper(*args, **kwargs):
+            func.func_globals[modname] = import_method_dependency(module,
+                                                                  stacklevel=2)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorate_method
