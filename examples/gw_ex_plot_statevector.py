24702671aeee2023b5c2c283780ff481182a019f
Duncan Macleod <duncan.macleod@ligo.org>
2014-06-03 17:20:37 -0500

    examples: fix typo

--- examples/gw_ex_plot_statevector.py
+++ examples/gw_ex_plot_statevector.py
@@ -42,7 +42,8 @@ bitmask = [
     'Stage 2 isolated',
     'Master switch ON',
     'Stage 1 WatchDog OK',
-    'Stage 2 WatchDog OK']
+    'Stage 2 WatchDog OK'
+]
 
 # get data
 data = StateVector.fetch('L1:ISI-ETMX_ODC_CHANNEL_OUT_DQ', 'May 22 2014 14:00', 'May 22 15:00', bitmask=bitmask)
