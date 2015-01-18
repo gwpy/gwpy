3606750513b913ebaf3801b2336a9a9d3796758b
joseph.areeda <joseph.areeda@ligo.org>
2015-01-18 09:15:16 -0800

    starting to process arguments

---
+++ gwpy/cli/coher.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) Joseph Areeda (2015)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+""" Coherence plots
+"""
+from CliProduct import CliProduct
+
+class Coher(CliProduct):
+
+    min_timeseries = 2
+
+    def get_action(self):
+        """Return the string used as "action" on command line."""
+        return 'coher'
+
+    def init_cli(self, parser):
+        """Set up the argument list for this product"""
+        self.arg_chan2(parser)
+        self.arg_freq(parser)
+        self.arg_plot(parser)
+        return
