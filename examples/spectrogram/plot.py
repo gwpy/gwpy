f761c997bbbb43930667a7aea5fb6157ee997fe5
Duncan Macleod <duncan.macleod@ligo.org>
2015-02-18 13:41:19 -0600

    examples: use 'rds' suffix for S6 h(t)

--- examples/spectrogram/plot.py
+++ examples/spectrogram/plot.py
@@ -28,7 +28,7 @@ __currentmodule__ = 'gwpy.spectrogram'
 # First, we import the :class:`~gwpy.timeseries.TimeSeries` and :meth:`~gwpy.timeseries.TimeSeries.fetch` the data:
 from gwpy.timeseries import TimeSeries
 gwdata = TimeSeries.fetch(
-    'H1:LDAS-STRAIN', 'September 16 2010 06:40', 'September 16 2010 06:50')
+    'H1:LDAS-STRAIN,rds', 'September 16 2010 06:40', 'September 16 2010 06:50')
 
 # Next, we can calculate a `Spectrogram` using the 
 # :meth:`~gwpy.timeseries.TimeSeries.spectrogram` method of the #
