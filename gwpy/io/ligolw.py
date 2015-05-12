094339b9f094e4d5cc7177647b5fe320b7c0f5a8
Duncan Macleod <duncan.macleod@ligo.org>
2015-05-12 13:22:46 -0500

    Revert "io.ligolw: use unlink to promote gc"
    This reverts commit 4985b461148ea5d916cb44eee17e9a722255ee9e.

--- gwpy/io/ligolw.py
+++ gwpy/io/ligolw.py
@@ -115,7 +115,6 @@ def table_from_file(f, tablename, columns=None, filt=None,
             gprint('%d rows remaining\n' % len(out))
     if columns is not None:
         tableclass.loadcolumns = _oldcols
-    xmldoc.unlink()
     return out
 
 
