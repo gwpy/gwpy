f387c1520aed08a5c4d28e264fe5ba4b7e39a885
Duncan Macleod <duncan.macleod@ligo.org>
2015-06-25 12:54:35 -0500

    tests: added a couple of new tests
        - also cleanup shared features of the timeseries tests module

--- gwpy/tests/timeseries.py
+++ gwpy/tests/timeseries.py
@@ -29,15 +29,15 @@ if sys.version_info < (2, 7):
 else:
     import unittest
 
-from numpy import (random, testing as nptest)
+import numpy
+from numpy import testing as nptest
 
 from astropy import units
 
-from gwpy.time import Time
+from ..time import Time
 
-from gwpy import version
-from gwpy.timeseries.core import TimeSeries
-from gwpy.timeseries.statevector import StateVector
+from .. import version
+from ..timeseries import (TimeSeries, StateVector)
 
 SEED = 1
 GPS_EPOCH = Time(0, format='gps', scale='utc')
@@ -60,8 +60,8 @@ class TimeSeriesTests(unittest.TestCase):
     SeriesClass = TimeSeries
 
     def setUp(self):
-        random.seed(SEED)
-        self.data = random.random(100)
+        numpy.random.seed(SEED)
+        self.data = numpy.random.random(100)
         self._ts = None
 
     def create_with_random_data(self, sample_rate=1, name='TEST CASE',
@@ -196,20 +196,24 @@ class TimeSeriesTests(unittest.TestCase):
         self.assertEquals(ts3.size, ts1.size+ts2.size)
         self.assertEquals(ts3.span, ts1.span+ts2.span)
         self.assertRaises(ValueError, ts3.append, ts1)
+        nptest.assert_array_equal(ts3.value[:ts1.size], ts1.value)
+        nptest.assert_array_equal(ts3.value[-ts2.size:], ts2.value)
 
     def test_prepend(self):
         """Test the `TimeSeries.prepend` method
         """
         ts1 = self.get_test_series()
-        ts2 = self.create_with_random_data(epoch=ts1.span[1])
+        ts2 = self.create_with_random_data(epoch=ts1.span[1]) * 2
         ts3 = ts2.prepend(ts1, inplace=False)
         self.assertEquals(ts3.epoch, ts1.epoch)
         self.assertEquals(ts3.size, ts1.size+ts2.size)
         self.assertEquals(ts3.span, ts1.span+ts2.span)
         self.assertRaises(ValueError, ts3.prepend, ts1)
+        nptest.assert_array_equal(ts3.value[:ts1.size], ts1.value)
+        nptest.assert_array_equal(ts3.value[-ts2.size:], ts2.value)
 
     def test_update(self):
-        """Test the `TimeSeries.update method`
+        """Test the `TimeSeries.update` method
         """
         ts1 = self.get_test_series()
         ts2 = self.create_with_random_data(epoch=ts1.span[1])[:ts1.size//2]
@@ -218,6 +222,42 @@ class TimeSeriesTests(unittest.TestCase):
         self.assertEquals(ts3.size, ts1.size)
         self.assertRaises(ValueError, ts3.update, ts1)
 
+    def test_resample(self):
+        """Test the `TimeSeries.resample` method
+        """
+        ts1 = self.create_with_random_data(sample_rate=100)
+        ts2 = ts1.resample(10)
+        self.assertEquals(ts2.sample_rate, ONE_HZ*10)
+
+    def test_pad(self):
+        """Test the `TimeSeries.pad` method
+        """
+        ts1 = self.get_test_series()
+        ts2 = ts1.pad(10)
+        self.assertEquals(ts2.size, ts1.size + 20)
+        nptest.assert_array_equal(
+            ts2.value,
+            numpy.concatenate((numpy.zeros(10), ts1.value, numpy.zeros(10))))
+        self.assertEquals(ts2.x0.value, ts1.x0.value - 10)
+        # test pre-pad
+        ts3 = ts1.pad((20, 10))
+        self.assertEquals(ts3.size, ts1.size + 30)
+        nptest.assert_array_equal(
+            ts3.value,
+            numpy.concatenate((numpy.zeros(20), ts1.value, numpy.zeros(10))))
+        self.assertEquals(ts3.x0.value, ts1.x0.value - 20)
+        # test bogus input
+        self.assertRaises(ValueError, ts1.pad, -1)
+
+    def test_to_from_lal(self):
+        ts = self.get_test_series()
+        try:
+            lalts = ts.to_lal()
+        except NotImplementedError as e:
+            self.skipTest(str(e))
+        ts2 = type(ts).from_lal(lalts)
+        self.assertEqual(ts, ts2)
+
 
 # -----------------------------------------------------------------------------
 
@@ -225,6 +265,11 @@ class StateVectorTests(TimeSeriesTests):
     """`~unittest.TestCase` for the `~gwpy.timeseries.StateVector` object
     """
     SeriesClass = StateVector
+
+    def setUp(self):
+        numpy.random.seed(SEED)
+        self.data = (numpy.random.random(100) * 1e5).astype('uint32')
+        self._ts = None
 
 
 if __name__ == '__main__':
