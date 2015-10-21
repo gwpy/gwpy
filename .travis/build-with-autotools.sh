c8910529ea83a6d3b1a963d9d2b8a80203be6f2e
Duncan Macleod <duncan.macleod@ligo.org>
2015-10-21 17:51:44 -0500

    .travis: attempt at container build for gwpy
    - install autotools dependencies FFTW3, LAL, LALFrame with
      build-with-autotools.sh script

---
+++ .travis/build-with-autotools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# build a package with autotools
+
+set -e
+
+tarball=$1
+target=$2
+
+builddir="build_$RANDOM"
+mkdir -p $builddir
+echo "Building into $builddir"
+# untar
+wget $tarball -O `basename $tarball`
+tar -zxf `basename $tarball` -C $builddir --strip-components=1
+cd $builddir
+if [ -f ./00boot ]; then
+    ./00boot
+fi
+./configure --prefix=`greadlink -f $target` --enable-silent-rules --quiet
+make
+make install
+
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${target}/lib/pkgconfig
