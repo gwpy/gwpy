d5af459b3e498c777c225b02e0fd971b4ca8f5c3
Duncan Macleod <duncan.macleod@ligo.org>
2015-11-06 07:47:05 -0600

    .travis: pass src build directory manually and cache

--- .travis/build-with-autotools.sh
+++ .travis/build-with-autotools.sh
@@ -4,8 +4,9 @@
 
 set -e
 
-tarball=$1
-shift
+builddir=$1
+tarball=$2
+shift 2
 configargs="$@"
 
 echo "----------------------------------------------------------------------"
@@ -14,15 +15,18 @@ echo "Installing from ${tarball}"
 # set install target to python prefix
 target=`python -c "import sys; print(sys.prefix)"`
 echo "Will install into ${target}"
-
-# move to build directory
-builddir="build_$RANDOM"
-mkdir -p $builddir
 echo "Building into $builddir"
 
-# untar
-wget $tarball --quiet -O `basename $tarball`
-tar -zxf `basename $tarball` -C $builddir --strip-components=1
+# dont rebuild from scratch if not required
+if [ -d $builddir ]; then
+    echo "Cached build directory found, not downloading tarball"
+else
+    mkdir -p $builddir
+    wget $tarball --quiet -O `basename $tarball`
+    tar -zxf `basename $tarball` -C $builddir --strip-components=1
+fi
+
+# always install and return
 cd $builddir
 if [ -f ./00boot ]; then
     ./00boot
@@ -30,6 +34,4 @@ fi
 ./configure --enable-silent-rules --quiet --prefix=$target $@
 make #-j
 make install
-
 cd -
-rm -rf ${builddir}
