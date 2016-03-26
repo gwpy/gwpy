29c5a2bd79c5891bbc9243d7e63a5b4b5b1942a9
Duncan Macleod <duncan.macleod@ligo.org>
2016-03-25 21:53:46 -0500

    tests: added a bunch of Channel{List} tests

--- gwpy/tests/test_detector.py
+++ gwpy/tests/test_detector.py
@@ -30,8 +30,7 @@ import numpy
 from astropy import units
 
 from gwpy import version
-from gwpy.detector import Channel
-from gwpy.utils import with_import
+from gwpy.detector import (Channel, ChannelList)
 from gwpy.detector.units import parse_unit
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
@@ -66,6 +65,52 @@ class ChannelTests(unittest.TestCase):
         self.assertEqual(new.unit, new2.unit)
         self.assertEqual(new.texname, new2.texname)
 
+    def test_parse_channel_name(self):
+        self.assertRaises(ValueError, Channel.parse_channel_name, 'blah')
+        valid = {
+            'ifo': 'X1',
+            'system': 'TEST',
+            'subsystem': 'CHANNEL',
+            'signal': 'NAME_PARSING',
+            'trend': 'rms',
+            'type': 'm-trend',
+        }
+        out = Channel.parse_channel_name(
+            'X1:TEST-CHANNEL_NAME_PARSING.rms,m-trend')
+        self.assertDictEqual(out, valid)
+        c = Channel('')
+        for attr in valid:
+            self.assertIsNone(getattr(c, attr))
+        c = Channel('X1:TEST-CHANNEL_NAME_PARSING.rms,m-trend')
+        for attr in valid:
+            self.assertEqual(valid[attr], getattr(c, attr))
+        self.assertEqual(c.name, 'X1:TEST-CHANNEL_NAME_PARSING.rms')
+        self.assertEqual(c.ndsname,
+                         'X1:TEST-CHANNEL_NAME_PARSING.rms,m-trend')
+
+    def test_property_frequency_range(self):
+        new = Channel('test', frequency_range=(1, 40))
+        self.assertIsInstance(new.frequency_range, units.Quantity)
+        self.assertListEqual(list(new.frequency_range.value), [1, 40])
+        self.assertIs(new.frequency_range.unit, units.Hz)
+        self.assertRaises(TypeError, Channel, '', frequency_range=1)
+
+    def test_property_safe(self):
+        new = Channel('')
+        self.assertIsNone(new.safe)
+        new.safe = True
+        self.assertTrue(new.safe)
+
+    def test_property_type_ndstype(self):
+        new = Channel('')
+        self.assertIsNone(new.ndstype)
+        new.type = 'm-trend'
+        self.assertEqual(new.ndstype, 16)
+        self.assertEqual(new.type, 'm-trend')
+        new.ndstype = 's-trend'
+        self.assertEqual(new.ndstype, 8)
+        self.assertEqual(new.type, 's-trend')
+
     def test_query(self):
         try:
             new = Channel.query(self.channel)
@@ -74,6 +119,10 @@ class ChannelTests(unittest.TestCase):
             if ('timed out' in msg.lower() or
                 'connection reset' in msg.lower()):
                 self.skipTest(msg)
+            raise
+        except ValueError as e:
+            if 'No JSON object could be decoded' in str(e):
+                self.skipTest(str(e))
             raise
         except Exception as e:
             try:
@@ -171,6 +220,56 @@ class UnitTest(unittest.TestCase):
         # test error
         self.assertRaises(ValueError, lalutils.to_lal_unit, 'blah')
         self.assertRaises(TypeError, lalutils.from_lal_unit,  'blah')
+
+
+class ChannelListTestCase(unittest.TestCase):
+    NAMES = ['X1:GWPY-CHANNEL_1', 'X1:GWPY-CHANNEL_2', 'X1:GWPY-CHANNEL_3']
+    SAMPLE_RATES = [1, 4, 8]
+    REAL_CHANNELS = ['L1:IMC-PWR_IN_OUT_DQ', 'H1:PSL-ODC_CHANNEL_OUT_DQ']
+
+    def setUp(self):
+        self.channels = [Channel(n, s) for n, s in
+                    zip(self.NAMES, self.SAMPLE_RATES)]
+
+    def create(self):
+        return ChannelList(self.channels)
+
+    def test_from_names(self):
+        cl = ChannelList.from_names(*self.NAMES)
+        self.assertListEqual(cl, map(Channel, self.NAMES))
+        cl2 = ChannelList.from_names(','.join(self.NAMES))
+        self.assertListEqual(cl, cl2)
+
+    def test_find(self):
+        cl = ChannelList.from_names(*self.NAMES)
+        self.assertEqual(cl.find(self.NAMES[2]), 2)
+        self.assertRaises(ValueError, cl.find, 'blah')
+
+    def test_sieve(self):
+        cl = self.create()
+        cl2 = cl.sieve(name='GWPY-CHANNEL')
+        self.assertListEqual(cl, cl2)
+        cl2 = cl.sieve(name='X1:GWPY-CHANNEL_2', exact_match=True)
+        self.assertIs(cl2[0], cl[1])
+        cl2 = cl.sieve(name='GWPY-CHANNEL', sample_range=[2, 16])
+        self.assertListEqual(cl2, cl[1:])
+
+    def test_query_nds2(self):
+        try:
+            import nds2
+        except ImportError as e:
+            self.skipTest(str(e))
+        try:
+            from gwpy.io import kerberos
+            kerberos.which('kinit')
+        except ValueError as e:
+            self.skipTest(str(e))
+        try:
+            cl = ChannelList.query_nds2(self.REAL_CHANNELS,
+                                        host='nds.ligo.caltech.edu')
+        except IOError as e:
+            self.skipTest(str(e))
+        self.assertEqual(len(cl), 5)
 
 
 if __name__ == '__main__':
