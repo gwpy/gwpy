022703d407264275de74ec326e19c8d772e13237
Duncan Macleod <duncan.macleod@ligo.org>
2016-04-26 17:15:44 -0500

    FrequencySeries.ifft: fixed bug

--- gwpy/frequencyseries/core.py
+++ gwpy/frequencyseries/core.py
@@ -163,8 +163,8 @@ class FrequencySeries(Series):
         # Undo normalization from TimeSeries.fft
         # The DC component does not have the factor of two applied
         # so we account for it here
-        dift[0] *= 2
-        dift = npfft.irfft(self.value * nout / 2)
+        dift = npfft.irfft(self.value * nout)
+        dift[1:] /= 2
         new = TimeSeries(dift, epoch=self.epoch, channel=self.channel,
                        unit=self.unit * units.Hertz, dx=1/self.dx/nout)
         return new
