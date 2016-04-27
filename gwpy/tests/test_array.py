418374cceb69b946f2cc16724ac1639cf91a4c43
Duncan Macleod <duncan.macleod@ligo.org>
2016-04-27 15:35:49 -0500

    Revert "tests: always print warnings for Series.crop"
    This reverts commit 58c5e127d661f5d3da8ae1df00f06f119f9da0dd.

--- gwpy/tests/test_array.py
+++ gwpy/tests/test_array.py
@@ -22,7 +22,6 @@
 import abc
 import os
 import tempfile
-import warnings
 
 import pytest
 from compat import unittest
@@ -233,12 +232,10 @@ class SeriesTestCase(CommonTests, unittest.TestCase):
         self.assertEqual(ts2.xspan[1], 20)
         nptest.assert_array_equal(ts2.value, ts.value[10:20])
         # check that warnings are printed for out-of-bounds
-        with warnings.catch_warnings():
-            warnings.simplefilter('always')
-            with pytest.warns(UserWarning) as warnrecord:
-                ts.crop(ts.xspan[0]-1, ts.xspan[1])
-                ts.crop(ts.xspan[0], ts.xspan[1]+1)
-            assert len(warnrecord) == 2
+        with pytest.warns(UserWarning):
+            ts.crop(ts.xspan[0]-1, ts.xspan[1])
+        with pytest.warns(UserWarning):
+            ts.crop(ts.xspan[0], ts.xspan[1]+1)
 
     def test_is_compatible(self):
         """Test the `Series.is_compatible` method
