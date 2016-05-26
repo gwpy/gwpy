3d7398bc6b213ec13f0e7a173f3f3e9ea428783d
Duncan Macleod <duncan.macleod@ligo.org>
2016-05-26 12:17:08 -0700

    SpectralVariance.__getitem__: handled corner-cases
    this code is rubbish, and should be looked at again

--- gwpy/frequencyseries/hist.py
+++ gwpy/frequencyseries/hist.py
@@ -139,10 +139,21 @@ class SpectralVariance(Array2D):
     # SpectralVariance methods
 
     def __getitem__(self, item):
-        if isinstance(item, tuple) and len(item) == 2:
+        if isinstance(item, int):
+            out = self.value[item].view(self._columnclass)
+            out.unit = self.unit
+            try:
+                out.xindex = self.yindex
+            except AttributeError:
+                pass
+            return out
+        if isinstance(item, tuple) and item[0] == slice(None, None, None):
+            out = self.value.__getitem__(item).view(self._rowclass)
+            out.xindex = self.xindex
+            return out
+        if (isinstance(item, tuple) and len(item) == 2):
             return self[item[0]][item[1]]
-        else:
-            return super(SpectralVariance, self).__getitem__(item)
+        return super(SpectralVariance, self).__getitem__(item)
     __getitem__.__doc__ = Array2D.__getitem__.__doc__
 
     @classmethod
