3f622c99a3d9bbfe6c5ce5aa4e004b2fa03898be
Duncan Macleod <duncan.macleod@ligo.org>
2016-11-30 16:34:40 -0600

    travis: always install LAL

--- .travis/build-src-dependencies.sh
+++ .travis/build-src-dependencies.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 
-# don't install if doing a 'minimal' build
+# install LAL first (always required)
+bash -e .travis/build-lal.sh
+
+# stop here if doing a minimal build
 [[ "$MINIMAL" = true ]] && return
+
+# -- install everything else
 
 # set paths for PKG_CONFIG
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${VIRTUAL_ENV}/lib/pkgconfig
 
 FAILURES=""
 
-# build a newer version of swig
-bash .travis/build-with-autotools.sh swig-${SWIG_VERSION}/${TRAVIS_PYTHON_VERSION} ${SWIG_} || FAILURES="$FAILURES swig"
-
-# build FFTW3 (double and float)
-bash .travis/build-with-autotools.sh fftw-${FFTW_VERSION}/${TRAVIS_PYTHON_VERSION} ${FFTW} --enable-shared=yes || FAILURES="$FAILURES fftw"
-bash .travis/build-with-autotools.sh fftw-${FFTW_VERSION}-float/${TRAVIS_PYTHON_VERSION} ${FFTW} --enable-shared=yes --enable-float || FAILURES="$FAILURES fftw-float"
-
-
 # build frame libraries
 bash .travis/build-with-autotools.sh ldas-tools-${LDAS_TOOLS_VERSION}/${TRAVIS_PYTHON_VERSION} ${LDAS_TOOLS} || FAILURES="$FAILURES ldas-tools"
-
 bash .travis/build-with-autotools.sh libframe-${LIBFRAME_VERSION}/${TRAVIS_PYTHON_VERSION} ${LIBFRAME} || FAILURES="$FAILURES libframe"
 
-
-
-# build LAL packages
-bash .travis/build-with-autotools.sh lal-${LAL_VERSION}/${TRAVIS_PYTHON_VERSION} ${LAL} --enable-swig-python || FAILURES="$FAILURES lal"
-
+# LALFrame
 bash .travis/build-with-autotools.sh lalframe-${LALFRAME_VERSION}/${TRAVIS_PYTHON_VERSION} ${LALFRAME} --enable-swig-python || FAILURES="$FAILURES lalframe"
 
-# build NDS2 client
+# NDS2 client
 bash .travis/build-with-autotools.sh nds2-client-${NDS2_CLIENT_VERSION}/${TRAVIS_PYTHON_VERSION} ${NDS2_CLIENT} --disable-swig-java --disable-mex-matlab || FAILURES="$FAILURES nds2-client"
 
 if [[ -n "${FAILURES+x}" ]]; then
