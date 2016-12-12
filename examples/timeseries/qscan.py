afb41d48e75f5fb5c97f48eb191e432f7c4ba8e3
Duncan Macleod <duncan.macleod@ligo.org>
2016-12-12 12:46:00 -0600

    examples: updated qscan example to use GW150914

--- examples/timeseries/qscan.py
+++ examples/timeseries/qscan.py
@@ -19,25 +19,29 @@
 
 """Generate the Q-transform of a `TimeSeries`
 
+One of the most useful tools for filtering and visualising short-duration
+features in a `TimeSeries` is the
+`Q-transform <https://en.wikipedia.org/wiki/Constant-Q_transform>`_.
+This is regularly used by the Detector Characterization working groups of
+the LIGO Scientific Collaboration and the Virgo Collaboration to produce
+high-resolution time-frequency maps of transient noise (glitches) and
+potential gravitational-wave signals.
+
+This algorithm was used to visualise the first ever gravitational-wave
+detection GW150914, so we can reproduce `that result (bottom panel of figure 1)
+<https://doi.org/10.1103/PhysRevLett.116.061102>`_ here.
 """
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 __currentmodule__ = 'gwpy.timeseries'
 
-# First, we identify the GPS time of interest:
-gps = 968654558
+# First, we need to download the `TimeSeries` record for the H1 strain
+# measurement from `LOSC <//losc.ligo.org>`_:
 
-# and use that to define the start and end times of our required data
-duration = 32
-start = int(round(gps - duration/2.))
-end = start + duration
-
-# next, we import the `TimeSeries` and fetch some open data from
-# `LOSC <//losc.ligo.org>`_:
 from gwpy.timeseries import TimeSeries
-data = TimeSeries.fetch_open_data('H1', start, end)
+data = TimeSeries.fetch_open_data('H1', 1126259446, 1126259478)
 
-# and next we generate the `~TimeSeries.q_transform` of these data:
+# Next, we generate the `~TimeSeries.q_transform` of these data:
 qspecgram = data.q_transform()
 
 # Now, we can plot the resulting `~gwpy.spectrogram.Spectrogram`, focusing on a
@@ -51,18 +55,18 @@ qspecgram = data.q_transform()
 #    `~matplotlib.axes.Axes.pcolormesh` and not any sort of image
 #    interpolation, mainly to support both linear and log scaling nicely)
 
-plot = qspecgram.crop(gps-.3, gps+.1).plot(figsize=[8, 6])
+gps = 1126259462.427
+plot = qspecgram.crop(gps-.15, gps+.05).plot(figsize=[8, 4])
 ax = plot.gca()
 ax.set_epoch(gps)
 ax.set_yscale('log')
 ax.set_xlabel('Time [milliseconds]')
-ax.set_ylim(50, 1000)
+ax.set_ylim(20, 500)
 ax.grid(True, axis='y', which='both')
 plot.add_colorbar(cmap='viridis', label='Normalized energy')
 plot.show()
 
-# I think we just detected a gravitational wave signal! But, before you
-# get too excited, this is an example of a 'blind injection', a simulated
-# signal introduced into the interferometer(s) in order to test the
-# detection process end-to-end.
-# For more details, see `here <http://www.ligo.org/scientists/GW100916/>`_.
+# Here we can clearly see the trace of a compact binary coalescence,
+# specifically a binary black hole coalescence!
+# For more details on this result, please see
+# http://www.ligo.org/science/Publication-GW150914/.
