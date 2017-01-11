66c5978e768bb1d3174683d24e3e4e339363ba96
Duncan Macleod <duncan.macleod@ligo.org>
2017-01-11 11:55:40 -0600

    tests: use mock to fake dqsegdb calls

---
+++ gwpy/tests/mockutils.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Duncan Macleod (2017)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Mock objects for GWpy tests
+"""
+
+__author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+
+
+# -- DQSEGDB calls ------------------------------------------------------------
+
+def mock_query_times(result, deactivated=False,
+                     active_indicates_ifo_badness=False, **kwargs):
+    """Build a mock of `dqsegdb.apicalls.dqsegdbQueryTimes` for testing
+    """
+
+    def query_times(protocol, server, ifo, name, version, request, start, end):
+        flag = '%s:%s:%d' % (ifo, name, version)
+        return {
+            'ifo': ifo,
+            'name': name,
+            'version': version,
+            'known': list(map(tuple, result[flag].known)),
+            'active': list(map(tuple, result[flag].active)),
+            'query_information': {},
+            'metadata': kwargs,
+        }, 'BOGUS_QUERY_STRING'
+
+    return query_times
