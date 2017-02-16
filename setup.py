e2c265b7039f271f5381d9479a0374fa798de387
Duncan Macleod <duncan.macleod@ligo.org>
2017-02-16 12:47:27 -0600

    removed support for python 2.6
    minimum supported version is now python 2.7, mainly because glue doesn't allow less than 2.7 as of 1.53.

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@
 """
 
 import sys
-if sys.version < '2.6':
-    raise ImportError("Python versions older than 2.6 are not supported.")
+if sys.version < '2.7':
+    raise ImportError("Python versions older than 2.7 are not supported.")
 
 import glob
 import hashlib
