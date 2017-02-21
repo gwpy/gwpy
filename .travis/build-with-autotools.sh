2d3ad4db776548c4953899a78931726147b51617
Duncan Macleod <duncan.macleod@ligo.org>
2017-02-21 10:22:10 -0600

    travis-ci: only ./configure if fresh build
    should allow make/make install to run quickly

--- .travis/build-with-autotools.sh
+++ .travis/build-with-autotools.sh
@@ -19,26 +19,31 @@ echo "Building into $builddir"
 
 # check for existing file
 if [ -f $builddir/.travis-src-file ] && [ `cat $builddir/.travis-src-file` == "$tarball" ]; then
-    echo "Cached build directory found, not downloading tarball"
+    echo "Cached build directory found, skippping to make..."
 else
+    # download tarball
     echo "New build requested, downloading tarball..."
     rm -rf $builddir/
     mkdir -p $builddir
     wget $tarball -O `basename $tarball`
     tar -xf `basename $tarball` -C $builddir --strip-components=1
     echo $tarball > $builddir/.travis-src-file
+
+    # boot and configure
+    cd $builddir
+    if [ -f ./00boot ]; then
+        ./00boot
+    elif [ -f ./autogen.sh ]; then
+        ./autogen.sh
+    fi
+    ./configure --enable-silent-rules --prefix=$target $@
 fi
 
-# always install and return
-cd $builddir
-if [ -f ./00boot ]; then
-    ./00boot
-elif [ -f ./autogen.sh ]; then
-    ./autogen.sh
-fi
-./configure --enable-silent-rules --prefix=$target $@
-make -j 2 --silent || make --silent
+# make and install
+make --silent
 make install --silent
+
+# finish
 cd -
 echo "----------------------------------------------------------------------"
 echo "Successfully installed `basename ${tarball}`"
