08c59f3f8447218215d851fa45761ba1604b34c0
Duncan Macleod <duncan.macleod@ligo.org>
2017-03-30 12:36:05 -0500

    io.gwf: added generic frame writing utilities
    uses frameCPP API

--- gwpy/io/gwf.py
+++ gwpy/io/gwf.py
@@ -16,14 +16,133 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
-"""I/O utilities for GWF files using the lalframe API
+"""I/O utilities for GWF files using the lalframe or frameCPP APIs
 """
 
 from ..time import to_gps
 from ..utils import (shell, with_import)
+from .cache import open_cache
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 
+# first 4 bytes of any valid GWF file (see LIGO-T970130 §4.3.1)
+GWF_SIGNATURE = 'IGWD'
+
+
+# -- i/o ----------------------------------------------------------------------
+
+def identify_gwf(origin, filepath, fileobj, *args, **kwargs):
+    """Identify a filename or file object as GWF
+
+    This function is overloaded in that it will also identify a cache file
+    as 'gwf' if the first entry in the cache contains a GWF file extension
+    """
+    # try and read file descriptor
+    if fileobj is not None:
+        loc = fileobj.tell()
+        fileobj.seek(0)
+        try:
+            if fileobj.read(4) == GWF_SIGNATURE:
+                return True
+        finally:
+            fileobj.seek(loc)
+    if filepath is not None:
+        if filepath.endswith('.gwf'):
+            return True
+        if filepath.endswith(('.lcf', '.cache')):
+            try:
+                cache = open_cache(filepath)
+            except IOError:
+                return False
+            else:
+                if cache[0].path.endswith('.gwf'):
+                    return True
+
+
+def write_frames(filename, frames, compression=257, compression_level=6):
+    """Write a list of frame objects to a file
+
+    **Requires:** |LDAStools.frameCPP|_
+
+    Parameters
+    ----------
+    filename : `str`
+        path to write into
+
+    frames : `list` of `LDAStools.frameCPP.FrameH`
+        list of frames to write into file
+
+    compression : `int`, optional
+        enum value for compression scheme, default is ``GZIP``
+
+    compression_level : `int`, optional
+        compression level for given scheme
+    """
+    from LDAStools import frameCPP
+    # open stream
+    stream = frameCPP.OFrameFStream(filename)
+    # write frames one-by-one
+    if isinstance(frames, frameCPP.FrameH):
+        frames = [frames]
+    for frame in frames:
+        stream.WriteFrame(frame, compression, compression_level)
+    # stream auto-closes (apparently)
+
+
+def create_frame(time=0, duration=None, name='gwpy', run=-1, ifos=[]):
+    """Create a new :class:`~LDAStools.frameCPP.FrameH`
+
+    **Requires:** |LDAStools.frameCPP|_
+
+    Parameters
+    ----------
+    time : `float`, optional
+        frame start time in GPS seconds
+
+    duration : `float`, optional
+        frame length in seconds
+
+    name : `str`, optional
+        name of project or other experiment description
+
+    run : `int`, optional
+        run number (number < 0 reserved for simulated data); monotonic for
+        experimental runs
+
+    ifos : `list` or `str`, optional
+        list of interferometer prefices (e.g. ``L1``) associated with this
+        frame
+
+    Returns
+    -------
+    frame : :class:`~LDAStools.frameCPP.FrameH`
+        the newly created frame header
+    """
+    from LDAStools import frameCPP
+
+    # create frame
+    frame = frameCPP.FrameH()
+
+    # add timing
+    gps = to_gps(time)
+    gps = frameCPP.GPSTime(gps.gpsSeconds, gps.gpsNanoSeconds)
+    frame.SetGTime(gps)
+    if duration is not None:
+        frame.SetDt(float(duration))
+
+    # add FrDetectors
+    for prefix in ifos:
+        idx = getattr(frameCPP, 'DETECTOR_LOCATION_%s' % prefix)
+        frame.AppendFrDetector(frameCPP.GetDetector(idx, gps))
+
+    # add descriptions
+    frame.SetName(name)
+    frame.SetRun(run)
+
+    return frame
+
+
+# -- utilities ----------------------------------------------------------------
 
 @with_import('lalframe')
 def num_channels(framefile):
