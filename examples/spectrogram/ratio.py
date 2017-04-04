75c0585b712f384a67275380e4abb14357d3a5de
Duncan Macleod <duncan.macleod@ligo.org>
2017-04-04 10:00:22 -0500

    examples: updated spectrogram examples with LOSC
    better to use public data wherever we can
    [ci skip]

--- examples/spectrogram/ratio.py
+++ examples/spectrogram/ratio.py
@@ -17,29 +17,51 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Plotting a whitened `Spectrogram`
+"""Plotting a normalised `~gwpy.spectrogram.Spectrogram`
 
-I would like to study the gravitational wave strain spectrogram around the time of an interesting simulated signal during the last science run (S6).
+The :ref:`previous example <example-spectrogram-plot>` showed how to
+generate and display a `~gwpy.spectrogram.Spectrogram of the LIGO-Hanford
+strain data around the GW150914 event.
+
+However, because of the shape of the LIGO sensitivity curve, picking out
+features in the most sensitive frequency band (a few hundred Hertz) is
+very hard.
+
+We can normalise our `~gwpy.spectrogram.Spectrogram` to highligh those
+features.
 """
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
-__currentmodule__ = 'gwpy.spectrogram'
+__currentmodule__ = 'gwpy.timeseries'
 
-# As with :doc:`previous example <plot>`, we import the
-# `~gwpy.timeseries.TimeSeries` class,
-# :meth:`~gwpy.timeseries.TimeSeries.get` the data, and calculate a 
-# `Spectrogram`
+# Again, we import the `TimeSeries` and call
+# :meth:`TimeSeries.fetch_open_data` the download the strain
+# data for the LIGO-Hanford interferometer
 from gwpy.timeseries import TimeSeries
-gwdata = TimeSeries.get('H1:LDAS-STRAIN', 'September 16 2010 06:40', 'September 16 2010 06:50')
-specgram = gwdata.spectrogram(5, fftlength=2, overlap=1) ** (1/2.)
+data = TimeSeries.fetch_open_data(
+    'H1', 'Sep 14 2015 09:45', 'Sep 14 2015 09:55')
 
-# To whiten the `specgram` we can use the :meth:`~Spectrogram.ratio` method
-# to divide by the overall median:
-medratio = specgram.ratio('median')
+# Next, we can calculate a `~gwpy.spectrogram.Spectrogram` using the
+# :meth:`spectrogram` method of the `TimeSeries` over a 2-second stride
+# with a 1-second FFT and # .5-second overlap (50%):
+specgram = data.spectrogram(2, fftlength=1, overlap=.5) ** (1/2.)
 
-# Finally, we make a plot:
-plot = medratio.plot(norm='log', vmin=0.1, vmax=10)
-plot.set_yscale('log')
-plot.set_ylim(40, 4096)
-plot.add_colorbar(label='Amplitude relative to median')
+# and can normalise it against the overall median ASD by calling the
+# :meth:`~gwpy.spectrogram.Spectrogram.ratio` method:
+
+normalised = specgram.ratio('median')
+
+# Finally, we can make a plot using the
+# :meth:`~gwpy.spectrogram.Spectrogram.plot` method
+plot = normalised.plot(norm='log', vmin=.1, vmax=10, cmap='Spectral_r')
+ax = plot.gca()
+ax.set_yscale('log')
+ax.set_ylim(10, 2000)
+plot.add_colorbar(label='Relative amplitude')
 plot.show()
+
+# Even with a normalised spectrogram, the resolution is such that a signal
+# as short as that of GW150914 is impossible to see. The
+# :ref:`next example <gwpy-example-spectrogram-spectrogram2>` uses
+# a high-resolution spectrogram method to zoom in around the exact time of
+# the signal.
