969e83ee08bb8e358fbb94baf4681704daa441c0
joseph.areeda <joseph.areeda@ligo.org>
2017-05-18 10:00:35 -0700

    add qtransform to cli plots + temp fix for  frq range to signal/qtransfor

--- gwpy/cli/qtransform.py
+++ gwpy/cli/qtransform.py
@@ -21,7 +21,12 @@
 
 """ Q-transform plots
 """
+
+
 from .cliproduct import CliProduct
+import types
+import math
+from pprint import pprint
 
 
 class Qtransform(CliProduct):
@@ -37,25 +42,36 @@ class Qtransform(CliProduct):
         self.arg_qxform(parser)
         self.arg_ax_linx(parser)
         self.arg_ax_ylf(parser)
-        self.arg_ax_intlog(parser)
+        self.arg_ax_intlin(parser)
         self.arg_imag(parser)
         self.arg_plot(parser)
         return
 
-    def post_arg(self,args):
+    def post_arg(self, args):
         """Derive standard args from our weird ones
         :type args: Namespace with command line arguments
         """
         event = float(args.gps)
         search = int(args.search)
+
         start = int(event - search / 2)
-        epoch = event - start
+        epoch = event
         args.start = [str(start)]
-        args.epoch = str(epoch)
+        args.epoch = ('%.3f' % epoch)
         args.duration = search
         args.chan = [[args.chan]]
         args.highpass = None
         args.lowpass = None
+        prng = args.plot
+        if isinstance(prng, types.ListType):
+            prange = float(prng[0])
+        elif prng:
+            prange = float(prng)
+        else:
+            prange = 0.5
+        args.xmin = ('%.3f' % (epoch - prange / 2))
+        args.xmax = ('%.3f' % (epoch + prange / 2))
+        self.verbose = args.verbose
 
     def get_ylabel(self, args):
         """Default text for y-axis label"""
@@ -82,16 +98,78 @@ class Qtransform(CliProduct):
 
     def gen_plot(self, args):
         """Generate the plot from time series and arguments"""
-        self.is_freq_plot = True
+        self.is_freq_plot = False   # not fft based
 
-        kwargs = {}     # optional args passed to qxfrm
-        kwargs['search'] = self.timeseries[0].dt * len(self.timeseries[0])
+        my_ts = self.timeseries[0]
+        self.title2 = ''
+        kwargs = dict()     # optional args passed to qxfrm
+        kwargs['search'] = my_ts.dt.value * len(my_ts)
         if args.qrange:
-            kwargs['qrange'] = args.qrange
+            kwargs['qrange'] = (float(args.qrange[0]), float(args.qrange[1]))
+            self.title2 += (' q-range [%.1f, %.1f] ' % (kwargs['qrange'][0], kwargs['qrange'][1]))
         if args.frange:
-            kwargs['frange'] = args.frange
+            kwargs['frange'] = (float(args.frange[0]), float(args.frange[1]))
+            self.title2 += (' f-range [%.0f, %.0f] ' % (kwargs['frange'][0], kwargs['frange'][1]))
         if args.nowhiten:
             kwargs['whiten'] = False
-        kwargs['gps'] = float(args.event)
+            self.title2 += 'not whitened'
+        else:
+            self.title2 += 'whitened '
+        kwargs['gps'] = float(args.gps)
+        kwargs['fres'] = 0.5
+        kwargs['tres'] = 0.002
+        self.title2 = ' central time % .3f ' % float(args.gps)
 
-        new_fs = args.sample_freq
+        new_fs = float(args.sample_freq)
+        cur_fs = my_ts.sample_rate.value
+
+        if cur_fs > new_fs:
+            my_ts = my_ts.resample(new_fs)
+            self.title2 = (' %.0f Hz resampled to %.0f. ' %
+                           (cur_fs, new_fs)) + self.title2
+            self.log(3,'Resampled input to %d Hz' % new_fs)
+        else:
+            new_fs = cur_fs
+
+        # if 'frange' in kwargs:
+        #     nyq = new_fs / 2
+        #     if kwargs['frange'][1] > nyq:
+        #         kwargs['frange'] = (kwargs['frange'][0], nyq-24)
+
+        if self.verbose >= 3:
+            print ('Q-transform args:')
+            pprint(kwargs)
+
+        self.result = my_ts.q_transform(**kwargs)
+        self.log(2, 'Result shape: %dx%d' % (self.result.shape[0], self.result.shape[1]))
+
+        pltargs=dict()
+        if args.cmap:
+            pltargs['cmap'] = args.cmap
+        # weirdness because we allow 2 ways to specify intensity range
+        imin = None
+        imax = None
+        if args.imin:
+            imin = float(args.imin)
+        if args.imax:
+            imax = float(args.imax)
+        if args.erange:
+            imin = float(args.erange[0])
+            imax = float(args.erange[1])
+
+        if imin:
+            pltargs['vmin'] = imin
+        if imax:
+            pltargs['vmax'] = imax
+
+        if self.verbose >= 3:
+            print ('Plot args:')
+            pprint(pltargs)
+
+        self.plot = self.result.plot(**pltargs)
+        self.scaleText = 'Normalized energy'
+
+
+        self.fmax = int(self.result.frequencies.max().value)
+        self.fmin = math.ceil(self.result.frequencies.min().value)
+        self.log(2, 'Frequncy range of result: %.2f - %.2f' % (self.fmin, self.fmax))
