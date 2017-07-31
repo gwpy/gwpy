5ddb0c8a29cab7b786b06125e618307c350f286c
Duncan Macleod <duncan.macleod@ligo.org>
2017-07-31 14:57:33 +0100

    tests: updated test of Channel{List}.query

--- gwpy/tests/test_detector.py
+++ gwpy/tests/test_detector.py
@@ -24,8 +24,6 @@ import os.path
 import warnings
 from tempfile import NamedTemporaryFile
 
-from six.moves import StringIO
-
 import pytest
 
 import numpy
@@ -391,10 +389,10 @@ class TestChannel(object):
             results = [channelinfo[name]]
         else:
             results = []
-        jsonresponse = StringIO(json.dumps({'results': results}))
+        jsonresponse = json.dumps({'results': results})
 
         # mock response and test parsing
-        with mock.patch('gwpy.utils.auth.request') as mocked:
+        with mock.patch('ligo.org.request') as mocked:
             mocked.return_value = jsonresponse
             if name == 'X1:TEST-CHANNEL':
                 c = self.TEST_CLASS.query(name)
