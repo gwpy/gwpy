4e88c01cbd5a2aecbb21af96a1f0d80b5b832bf7
Duncan Macleod <duncan.macleod@ligo.org>
2017-08-24 04:30:00 -0700

    examples: updated Rayleigh examples to use GW151226

--- examples/spectrogram/rayleigh.py
+++ examples/spectrogram/rayleigh.py
@@ -19,26 +19,30 @@
 
 """Plotting a `Spectrogram` of the Rayleigh statistic
 
-I would like to study the gravitational wave strain spectrogram around the time of an interesting simulated signal during the last science run (S6).
+As described in :ref:`gwpy-example-frequencyseries-rayleigh`, the Rayleigh statistic can be used to study non-Gaussianity in a timeseries. We can study the time variance of these features by plotting a time-frequency spectrogram where we calculate the Rayleigh statistic for each time bin.
 """
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 __currentmodule__ = 'gwpy.spectrogram'
 
-# First, we import the :class:`~gwpy.timeseries.TimeSeries` and :meth:`~gwpy.timeseries.TimeSeries.get` the data:
+# To demonstate this, we can load some data from the LIGO Livingston
+# intereferometer around the time of the GW151226 gravitational wave detection:
+
 from gwpy.timeseries import TimeSeries
-gwdata = TimeSeries.get(
-    'H1:LDAS-STRAIN', 'September 16 2010 06:40', 'September 16 2010 06:50')
+gwdata = TimeSeries.fetch_open_data('L1', 'Dec 26 2015 03:37',
+                                    'Dec 26 2015 03:47', verbose=True)
 
 # Next, we can calculate a Rayleigh statistic `Spectrogram` using the 
-# :meth:`~gwpy.timeseries.TimeSeries.rayleigh_spectrogram` method of the #
+# :meth:`~gwpy.timeseries.TimeSeries.rayleigh_spectrogram` method of the
 # `~gwpy.timeseries.TimeSeries` and a 5-second stride with a 2-second FFT and 
 # 1-second overlap (50%):
 rayleigh = gwdata.rayleigh_spectrogram(5, fftlength=2, overlap=1)
 
 # and can make a plot using the :meth:`~Spectrogram.plot` method
 plot = rayleigh.plot(norm='log', vmin=0.25, vmax=4)
-plot.set_yscale('log')
-plot.set_ylim(40, 4000)
-plot.add_colorbar(cmap='BrBG_r', label=r'Rayleigh statistic')
+ax = plot.gca()
+ax.set_yscale('log')
+ax.set_ylim(30, 1500)
+ax.set_title('Sensitivity of LIGO-Livingston around GW151226')
+plot.add_colorbar(cmap='coolwarm', label='Rayleigh statistic')
 plot.show()
