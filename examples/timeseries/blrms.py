571730911502521866be69c50267f76f82337852
Duncan Macleod <duncan.macleod@ligo.org>
2017-09-13 13:29:29 +0100

    examples: fixed two text issues with blrms.py

--- examples/timeseries/blrms.py
+++ examples/timeseries/blrms.py
@@ -52,12 +52,13 @@ llo = TimeSeriesDict.get([c % 'L1' for c in channels],
 # instrument:
 plot = TimeSeriesPlot(lho, llo)
 ax1, ax2 = plot.axes
-for ifo, ax in zip(('H1', 'L1'), (ax1, ax2)):
+for ifo, ax in zip(('Hanford', 'Livingston'), (ax1, ax2)):
    ax.legend(['X', 'Y', 'Z'])
    ax.set_yscale('log')
-   ax.text(1.02, 0.5, ifo, ha='left', va='center', transform=ax.transAxes,
+   ax.text(1.01, 0.5, ifo, ha='left', va='center', transform=ax.transAxes,
            fontsize=18)
 ax1.set_ylabel('$1-3$\,Hz motion [nm/s]', y=-0.1)
+ax2.set_ylabel('')
 ax1.set_title('Magnitude 7.1 earthquake impact on LIGO')
 plot.show()
 
