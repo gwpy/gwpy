1802714f8cb26c019d288025341108f502db1274
Duncan Macleod <duncan.macleod@ligo.org>
2017-10-19 14:06:48 +0100

    major rewrite of LIGO_LW XML interfaces
    - {Event}Table now sees a single 'ligolw' format, with a ``tablename=`` keyword
    - io.ligolw includes more utilities for interacting with LIGO_LW objects directly

--- gwpy/io/ligolw.py
+++ gwpy/io/ligolw.py
@@ -23,56 +23,174 @@ an 'io' subdirectory of the containing directory for that class.
 """
 
 import os.path
-import warnings
+from functools import wraps
 
 from six import string_types
 
-from ..utils import gprint
 from .cache import (file_list, FILE_LIKE)
-from .utils import identify_factory
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+
+XML_SIGNATURE = b'<?xml'
+LIGOLW_SIGNATURE = b'<!doctype ligo_lw'
 
 
 # -- content handling ---------------------------------------------------------
 
-def get_partial_contenthandler(table):
-    """Build a `~glue.ligolw.ligolw.PartialLIGOLWContentHandler` for this table
+def get_partial_contenthandler(element):
+    """Build a `PartialLIGOLWContentHandler` to read only this element
 
     Parameters
     ----------
-    table : `type`
-        the table class to be read
+    element : `type`
+        the element class to be read, subclass of
+        :class:`~glue.ligolw.ligolw.Element`
 
     Returns
     -------
     contenthandler : `type`
-        a subclass of `~glue.ligolw.ligolw.PartialLIGOLWContentHandler` to
-        read only the given `table`
+        a subclass of :class:`~glue.ligolw.ligolw.PartialLIGOLWContentHandler`
+        to read only the given `element`
     """
     from glue.ligolw.ligolw import PartialLIGOLWContentHandler
+    from glue.ligolw.table import Table
 
-    def _element_filter(name, attrs):
-        return table.CheckProperties(name, attrs)
+    if issubclass(element, Table):
+        def _element_filter(name, attrs):
+            # pylint: disable=unused-argument
+            return element.CheckProperties(name, attrs)
+    else:
+        def _element_filter(name, attrs):
+            # pylint: disable=unused-argument
+            return name == element.tagName
 
-    class _ContentHandler(PartialLIGOLWContentHandler):
+    return build_content_handler(PartialLIGOLWContentHandler, _element_filter)
+
+
+def get_filtering_contenthandler(element):
+    """Build a `FilteringLIGOLWContentHandler` to exclude this element
+
+    Parameters
+    ----------
+    element : `type`, subclass of :class:`~glue.ligolw.ligolw.Element`
+        the element to exclude (and its children)
+
+    Returns
+    -------
+    contenthandler : `type`
+        a subclass of
+        :class:`~glue.ligolw.ligolw.FilteringLIGOLWContentHandler` to
+        exclude an element and its children
+    """
+    from glue.ligolw.ligolw import FilteringLIGOLWContentHandler
+    from glue.ligolw.table import Table
+
+    if issubclass(element, Table):
+        def _element_filter(name, attrs):
+            # pylint: disable=unused-argument
+            return ~element.CheckProperties(name, attrs)
+    else:
+        def _element_filter(name, attrs):
+            # pylint: disable=unused-argument
+            return name != element.tagName
+
+    return build_content_handler(FilteringLIGOLWContentHandler,
+                                 _element_filter)
+
+
+def build_content_handler(parent, filter_func):
+    """Build a `~xml.sax.handler.ContentHandler` with a given filter
+    """
+    from glue.ligolw.lsctables import use_in
+
+    class _ContentHandler(parent):
+        # pylint: disable=too-few-public-methods
         def __init__(self, document):
-            super(_ContentHandler, self).__init__(document, _element_filter)
+            super(_ContentHandler, self).__init__(document, filter_func)
 
-    return _ContentHandler
+    return use_in(_ContentHandler)
 
 
 # -- reading ------------------------------------------------------------------
 
-def table_from_file(f, tablename, columns=None, filt=None,
-                    contenthandler=None, nproc=1, verbose=False):
-    """Read a `~glue.ligolw.table.Table` from a LIGO_LW file.
+def read_ligolw(source, contenthandler=None, verbose=False,
+                non_lsc_tables_ok=True):
+    """Read one or more LIGO_LW format files
 
     Parameters
     ----------
-    f : `file`, `str`, `CacheEntry`, `list`, `Cache`
+    source : `str`, `file`, `list`
+        one or more open files or file paths to read
+
+    contenthandler : `~xml.sax.handler.ContentHandler`, optional
+        content handler used to parse document
+
+    verbose : `bool`, optional
+        be verbose when reading files, default: `False`
+
+    non_lsc_tables_ok : `bool`, optional
+        if `False` error on unrecognised tables in documents, default: `True`
+
+    Returns
+    -------
+    xmldoc : :class:`~glue.ligolw.ligolw.Document`
+        the document object as parsed from the file(s)
+    """
+    from glue.ligolw.ligolw import (Document, LIGOLWContentHandler)
+    from glue.ligolw.utils.ligolw_add import ligolw_add
+    from glue.ligolw.lsctables import use_in
+
+    # set default content handler
+    if contenthandler is None:
+        contenthandler = use_in(LIGOLWContentHandler)
+
+    # read one or more files into a single Document
+    return ligolw_add(Document(), file_list(source),
+                      contenthandler=contenthandler, verbose=verbose,
+                      non_lsc_tables_ok=non_lsc_tables_ok)
+
+
+def with_read_ligolw(func=None, contenthandler=None):
+    """Decorate a LIGO_LW-reading function to open a filepath if needed
+
+    ``func`` should be written to presume a
+    :class:`~glue.ligolw.ligolw.Document` as the first positional argument
+    """
+    def decorator(func_):
+        # pylint: disable=missing-docstring
+        @wraps(func_)
+        def decorated_func(source, *args, **kwargs):
+            # pylint: disable=missing-docstring
+            from glue.ligolw.ligolw import Document
+            if not isinstance(source, Document):
+                read_kw = {
+                    'contenthandler': kwargs.pop('contenthandler',
+                                                 contenthandler),
+                    'verbose': kwargs.pop('verbose', False),
+                    'non_lsc_tables_ok': kwargs.pop('non_lsc_tables_ok', True),
+                }
+                return func_(read_ligolw(source, **read_kw), *args, **kwargs)
+            return func_(source, *args, **kwargs)
+
+        return decorated_func
+
+    if func is not None:
+        return decorator(func)
+    return decorator
+
+
+# -- reading ------------------------------------------------------------------
+
+def read_table(source, tablename=None, columns=None, contenthandler=None,
+               **kwargs):
+    """Read a `~glue.ligolw.table.Table` from one or more LIGO_LW files
+
+    Parameters
+    ----------
+    source : `Document`, `file`, `str`, `CacheEntry`, `list`, `Cache`
         object representing one or more files. One of
 
+        - a LIGO_LW :class:`~glue.ligolw.ligolw.Document`
         - an open `file`
         - a `str` pointing to a file path on disk
         - a formatted `~lal.utils.CacheEntry` representing one file
@@ -81,88 +199,95 @@ def table_from_file(f, tablename, columns=None, filt=None,
 
     tablename : `str`
         name of the table to read.
+
     columns : `list`, optional
         list of column name strings to read, default all.
-    filt : `function`, optional
-        function by which to `filter` events. The callable must accept as
-        input a row of the table event and return `True`/`False`.
-    contenthandler : `~glue.ligolw.ligolw.LIGOLWContentHandler`
+
+    contenthandler : `~xml.sax.handler.ContentHandler`, optional
         SAX content handler for parsing LIGO_LW documents.
+
+    **kwargs
+        other keyword arguments are passed to `~gwpy.io.ligolw.read_ligolw`
 
     Returns
     -------
-    table : `~glue.ligolw.table.Table`
-        `Table` of data with given columns filled
+    table : :class:`~glue.ligolw.table.Table`
+        `Table` of data
     """
     from glue.ligolw.ligolw import Document
     from glue.ligolw import (table, lsctables)
-    from glue.ligolw.utils.ligolw_add import ligolw_add
 
-    # find table class
-    tableclass = lsctables.TableByName[table.Table.TableName(tablename)]
+    # get content handler to read only this table (if given)
+    if tablename is not None:
+        tableclass = lsctables.TableByName[table.Table.TableName(tablename)]
+        if contenthandler is None:
+            contenthandler = get_partial_contenthandler(tableclass)
 
-    # get content handler
-    if contenthandler is None:
-        contenthandler = get_partial_contenthandler(tableclass)
-
-    # allow cache multiprocessing
-    if nproc != 1:
-        return tableclass.read(f, columns=columns,
-                               contenthandler=contenthandler,
-                               nproc=nproc, format='cache')
-
-    lsctables.use_in(contenthandler)
-
-    # set columns to read
-    if columns is not None:
+        # overwrite loading column names to get just what was asked for
         _oldcols = tableclass.loadcolumns
-        tableclass.loadcolumns = columns
+        if columns is not None:
+            tableclass.loadcolumns = columns
 
-    # generate Document and populate
-    files = file_list(f)
-    xmldoc = Document()
-    ligolw_add(xmldoc, files, non_lsc_tables_ok=True,
-               contenthandler=contenthandler, verbose=verbose)
+    # read document
+    if isinstance(source, Document):
+        xmldoc = source
+    else:
+        try:
+
+            xmldoc = read_ligolw(source, contenthandler=contenthandler,
+                                 **kwargs)
+        finally:  # reinstate original set of loading column names
+            if tablename is not None:
+                tableclass.loadcolumns = _oldcols
+
+    # now find the right table
+    if tablename is None:
+        tables = list_tables(xmldoc)
+        if not tables:
+            raise ValueError("No tables found in LIGO_LW document(s)")
+        if len(tables) > 1:
+            tlist = "'{}'".format("', '".join(tables))
+            raise ValueError("Multiple tables found in LIGO_LW document(s), "
+                             "please specify the table to read via the "
+                             "``tablename=`` keyword argument. The following "
+                             "tables were found: {}".format(tlist))
+        tableclass = lsctables.TableByName[table.Table.TableName(tables[0])]
 
     # extract table
-    out = tableclass.get_table(xmldoc)
-    if verbose:
-        gprint('%d rows found in %s table' % (len(out), out.tableName))
-
-    # filter output
-    if filt:
-        if verbose:
-            gprint('filtering rows ...', end=' ')
-        try:
-            out_ = out.copy()
-        except AttributeError:
-            out_ = table.new_from_template(out)
-        out_.extend(filter(filt, out))
-        out = out_
-        if verbose:
-            gprint('%d rows remaining\n' % len(out))
-
-    # reset loadcolumns and return
-    if columns is not None:
-        tableclass.loadcolumns = _oldcols
-
-    return out
+    return tableclass.get_table(xmldoc)
 
 
 # -- writing ------------------------------------------------------------------
 
-def open_xmldoc(f, **kwargs):
+def open_xmldoc(fobj, **kwargs):
     """Try and open an existing LIGO_LW-format file, or create a new Document
+
+    Parameters
+    ----------
+    fobj : `str`, `file`
+        file path or open file object to read
+
+    **kwargs
+        other keyword arguments to pass to
+        :func:`~glue.ligolw.utils.load_filename`, or
+        :func:`~glue.ligolw.utils.load_fileobj` as appropriate
+
+    Returns
+    --------
+    xmldoc : :class:`~glue.ligolw.ligolw.Document`
+        either the `Document` as parsed from an existing file, or a new, empty
+        `Document`
     """
     from glue.ligolw.lsctables import use_in
-    from glue.ligolw.ligolw import Document
+    from glue.ligolw.ligolw import (Document, LIGOLWContentHandler)
     from glue.ligolw.utils import load_filename, load_fileobj
-    use_in(kwargs['contenthandler'])
     try:  # try and load existing file
-        if isinstance(f, string_types):
-            return load_filename(f, **kwargs)
-        if isinstance(f, FILE_LIKE):
-            return load_fileobj(f, **kwargs)[0]
+        if isinstance(fobj, string_types):
+            kwargs.setdefault('contenthandler', use_in(LIGOLWContentHandler))
+            return load_filename(fobj, **kwargs)
+        if isinstance(fobj, FILE_LIKE):
+            kwargs.setdefault('contenthandler', use_in(LIGOLWContentHandler))
+            return load_fileobj(fobj, **kwargs)[0]
     except (OSError, IOError):  # or just create a new Document
         return Document()
 
@@ -208,7 +333,7 @@ def write_tables_to_document(xmldoc, tables, overwrite=False):
     for table in tables:
         try:  # append new data to existing table
             old = lsctables.TableByName[
-                table.TableName(table.tableName)].get_table(xmldoc)
+                table.TableName(table.Name)].get_table(xmldoc)
         except ValueError:  # or create a new table
             llw.appendChild(table)
         else:
@@ -222,15 +347,15 @@ def write_tables_to_document(xmldoc, tables, overwrite=False):
     return xmldoc
 
 
-def write_tables(f, tables, append=False, overwrite=False, **kwargs):
+def write_tables(target, tables, append=False, overwrite=False, **kwargs):
     """Write an LIGO_LW table to file
 
     Parameters
     ----------
-    f : `str`, `file`, :class:`~glue.ligolw.ligolw.Document`
+    target : `str`, `file`, :class:`~glue.ligolw.ligolw.Document`
         the file or document to write into
 
-    tables : `list` of :class:`~glue.ligolw.table.Table`
+    tables : `list`, `tuple` of :class:`~glue.ligolw.table.Table`
         the tables to write
 
     append : `bool`, optional, default: `False`
@@ -239,21 +364,27 @@ def write_tables(f, tables, append=False, overwrite=False, **kwargs):
     overwrite : `bool`, optional, default: `False`
         if `True`, delete an existing instance of the table type, otherwise
         append new rows
+
+    **kwargs
+        other keyword arguments to pass to
+        :func:`~glue.ligolw.utils.load_filename`, or
+        :func:`~glue.ligolw.utils.load_fileobj` as appropriate
     """
     from glue.ligolw.ligolw import (Document, LIGO_LW, LIGOLWContentHandler)
     from glue.ligolw import utils as ligolw_utils
 
     # allow writing directly to XML
-    if isinstance(f, (Document, LIGO_LW)):
-        xmldoc = f
+    if isinstance(target, (Document, LIGO_LW)):
+        xmldoc = target
     # open existing document, if possible
     elif append and not overwrite:
         xmldoc = open_xmldoc(
-            f, contenthandler=kwargs.pop('contenthandler',
-                                         LIGOLWContentHandler))
+            target, contenthandler=kwargs.pop('contenthandler',
+                                              LIGOLWContentHandler))
     # fail on existing document and not overwriting
-    elif not overwrite and isinstance(f, string_types) and os.path.isfile(f):
-        raise IOError("File exists: %s" % f)
+    elif (not overwrite and isinstance(target, string_types) and
+          os.path.isfile(target)):
+        raise IOError("File exists: {}".format(target))
     else:  # or create a new document
         xmldoc = Document()
 
@@ -261,14 +392,84 @@ def write_tables(f, tables, append=False, overwrite=False, **kwargs):
     write_tables_to_document(xmldoc, tables, overwrite=overwrite)
 
     # write file
-    if isinstance(f, string_types):
-        kwargs.setdefault('gz', f.endswith('.gz'))
-        ligolw_utils.write_filename(xmldoc, f, **kwargs)
-    elif not isinstance(f, Document):
-        kwargs.setdefault('gz', f.name.endswith('.gz'))
-        ligolw_utils.write_fileobj(xmldoc, f, **kwargs)
+    if isinstance(target, string_types):
+        kwargs.setdefault('gz', target.endswith('.gz'))
+        ligolw_utils.write_filename(xmldoc, target, **kwargs)
+    elif isinstance(target, FILE_LIKE):
+        kwargs.setdefault('gz', target.name.endswith('.gz'))
+        ligolw_utils.write_fileobj(xmldoc, target, **kwargs)
+
+
+# -- utilities ----------------------------------------------------------------
+
+def list_tables(source):
+    # pylint: disable=line-too-long
+    """List the names of all tables in this file(s)
+
+    Parameters
+    ----------
+    source : `file`, `str`, :class:`~glue.ligolw.ligolw.Document`, `list`
+        one or more open files, file paths, or LIGO_LW `Document`s
+
+    Examples
+    --------
+    >>> from gwpy.io.ligolw import list_tables
+    >>> print(list_tables('H1-LDAS_STRAIN-968654552-10.xml.gz'))
+    ['process', 'process_params', 'sngl_burst', 'search_summary', 'segment_definer', 'segment_summary', 'segment']
+    """  # nopep8
+    from glue.ligolw.ligolw import (Document, Stream)
+    from glue.ligolw.table import Table
+
+    # read file object
+    if isinstance(source, Document):
+        xmldoc = source
+    else:
+        filt = get_filtering_contenthandler(Stream)
+        xmldoc = read_ligolw(source, contenthandler=filt)
+
+    # get list of table names
+    tables = []
+    for tbl in xmldoc.childNodes[0].childNodes:
+        if isinstance(tbl, Table):
+            tables.append(tbl.TableName(tbl.Name))
+    return tables
 
 
 # -- identify -----------------------------------------------------------------
 
-identify_ligolw = identify_factory('xml', 'xml.gz')
+def is_ligolw(origin, filepath, fileobj, *args, **kwargs):
+    """Identify a file object as LIGO_LW-format XML
+    """
+    # pylint: disable=unused-argument
+    if fileobj is not None:
+        loc = fileobj.tell()
+        fileobj.seek(0)
+        try:
+            line1 = fileobj.readline().lower()
+            line2 = fileobj.readline().lower()
+            return (line1.startswith(XML_SIGNATURE) and
+                    line2.startswith(LIGOLW_SIGNATURE))
+        finally:
+            fileobj.seek(loc)
+    try:
+        from glue.ligolw.ligolw import Element
+    except ImportError:
+        return False
+    else:
+        return len(args) > 0 and isinstance(args[0], Element)
+
+
+def is_xml(origin, filepath, fileobj, *args, **kwargs):
+    """Identify a file object as XML (any format)
+    """
+    # pylint: disable=unused-argument
+    if fileobj is not None:
+        loc = fileobj.tell()
+        fileobj.seek(0)
+        try:
+            sig = fileobj.read(5).lower()
+            return sig == XML_SIGNATURE
+        finally:
+            fileobj.seek(loc)
+    elif filepath is not None:
+        return filepath.endswith(('.xml', '.xml.gz'))
