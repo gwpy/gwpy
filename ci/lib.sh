00405d35fe9a9a55c882c67de48e204960efa654
Duncan Macleod <duncan.macleod@ligo.org>
2017-10-31 12:27:15 +0000

    ci: rebuilt travis CI config to use docker
    allows testing on the LIGO reference OSs

---
+++ ci/lib.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright (C) Duncan Macleod (2017)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# Library functions for CI builds
+#
+
+[ -z ${DOCKER_IMAGE} ] && GWPY_PATH="`pwd`/" || GWPY_PATH="/gwpy/"
+
+ci_run() {
+    if [ -z ${DOCKER_IMAGE} ]; then  # execute function normally
+        eval "$@" || return 1
+    else  # execute function in docker container
+        cd ${GWPY_PATH}
+        docker exec -it ${GWPY_CI} sh -xec "$@" || return 1
+    fi
+    return 0
+}
