22206f65a71af15e1253a30e8a5a977f64657136
Duncan Macleod <duncan.macleod@ligo.org>
2017-10-31 12:27:15 +0000

    ci: rebuilt travis CI config to use docker
    allows testing on the LIGO reference OSs

---
+++ ci/deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Copyright (C) Duncan Macleod (2017)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+[ -z ${TRAVIS_SECURE_ENV_VARS} ] && return 0
+
+#
+# Build system packages
+#
+
+GWPY_VERSION=`python setup.py version | grep Version | cut -d\  -f2`
+
+if [ "${GWPY_CI}" == "gwpy-debian" ]; then
+    # prepare the tarball
+    pip install stdeb
+    python changelog.py --changelog-format=deb > debian/changelog
+    python setup.py sdist
+
+    # make the debian package
+    mkdir -p dist/debian
+    cd dist/debian
+    cp ../gwpy-${GWPY_VERSION}.tar.gz ../gwpy_${GWPY_VERSION}.orig.tar.gz
+    dpkg-buildpackage -us -uc
+
+    # install the deb
+    sudo dpkg -i ../python-gwpy_${GWPY_VERSION}-1_all.deb
+
+elif [ "${GWPY_CI}" == "gwpy-el7" ]; then
+    # build the RPM
+    python setup.py bdist_rpm --changelog=`python changelog.py`
+
+    # install the rpm
+    sudo rpm -ivh dist/gwpy-${GWPY_VERSION}-1.noarch.rpm
+
+fi
---
+++ ci/docker-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright (C) Duncan Macleod (2017)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# Configure the relevant docker container for this CI job, then install
+# the dependencies as system packages
+#
+
+if [ -z ${DOCKER_IMAGE} ]; then
+    echo "Not a docker install, skipping docker configuration"
+else
+    sudo apt-get update -yqq
+    sudo docker pull ${DOCKER_IMAGE}
+    sudo docker run \
+        --rm=true \
+        --detach \
+        --name "${GWPY_CI}" \
+        --volume `pwd`:/gwpy:rw \
+        ${DOCKER_IMAGE}
+    sleep 10
+fi
