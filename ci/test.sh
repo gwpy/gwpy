703499e995c7ab00de0cc6e780a7e4879c0533a6
Duncan Macleod <duncan.macleod@ligo.org>
2017-11-01 20:59:23 +0000

    ci: fixed bug in test script

--- ci/test.sh
+++ ci/test.sh
@@ -16,8 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
-. ci/lib.sh
 cd ${GWPY_PATH}
+
+. ci/lib.sh
 
 set -x
 set -e
