13016249552c0885e0c3c944238c4588dea61a83
Duncan Macleod <duncan.macleod@ligo.org>
2017-11-01 10:24:51 +0000

    ci: separate builds for each OS into scripts

---
+++ ci/install-debian.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Copyright (C) Duncan Macleod (2017)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# Build Debian package
+#
+
+GWPY_VERSION=${GWPY_VERSION/+/-}
+
+# install build dependencies
+apt-get update -yqq
+apt-get install -yqq \
+    debhelper \
+    ${PYPKG_PREFIX} \
+    ${PYPKG_PREFIX}-all \
+    ${PYPKG_PREFIX}-pip
+
+# install build helpers
+${PIP} install stdeb GitPython
+
+# prepare the tarball
+${PYTHON} changelog.py --start-tag "v0.5" > debian/changelog
+${PYTHON} setup.py sdist
+
+# make the debian package
+mkdir -p dist/debian
+pushd dist/debian
+cp ../gwpy-${GWPY_VERSION}.tar.gz ../gwpy_${GWPY_VERSION}.orig.tar.gz
+tar -xf ../gwpy_${GWPY_VERSION}.orig.tar.gz --strip-components=1
+dpkg-buildpackage -us -uc
+popd
+
+# install the deb
+dpkg -i dist/python-gwpy_${GWPY_VERSION}-1_all.deb
+
+# install system-level extras
+apt-get install -y \
+    ${PYPKG_PREFIX}-nds2-client \
+    ${PYPKG_PREFIX}-h5py \
+|| true
