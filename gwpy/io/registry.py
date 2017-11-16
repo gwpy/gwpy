09ac22dc5aa4ea7a558f194dffaf2e3b91f45c28
Duncan Macleod <duncan.macleod@ligo.org>
2017-11-16 10:24:23 +0000

    io.registry: appeased pylint

--- gwpy/io/registry.py
+++ gwpy/io/registry.py
@@ -17,16 +17,20 @@
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
 """I/O registry extensions on top of `astropy.io.registry`
+
+This module imports a subset of the useful functions from
+:mod:`astropy.io.registry` for convenience.
 """
 
 from functools import wraps
 
-from astropy.io.registry import *
+from astropy.io.registry import (  # pylint: disable=unused-import
+    register_identifier as astropy_register_identifier,
+    register_reader,
+    register_writer,
+)
 
 from .cache import file_list
-
-# record astropy origin registry_identifier
-astropy_register_identifier = register_identifier
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 
@@ -40,6 +44,7 @@ def identify_with_list(identifier):
     """
     @wraps(identifier)
     def decorated_func(origin, filepath, fileobj, *args, **kwargs):
+        # pylint: disable=missing-docstring
         try:
             filepath = file_list(filepath)[0]
         except ValueError:
@@ -49,7 +54,7 @@ def identify_with_list(identifier):
                 except (IndexError, ValueError):
                     pass
                 else:
-                    if len(files):
+                    if files:
                         filepath = files[0]
         except IndexError:
             pass
@@ -58,6 +63,7 @@ def identify_with_list(identifier):
 
 
 def register_identifier(data_format, data_class, identifier, force=False):
+    # pylint: disable=missing-docstring
     return astropy_register_identifier(
         data_format, data_class, identify_with_list(identifier), force=force)
 register_identifier.__doc__ = astropy_register_identifier.__doc__
