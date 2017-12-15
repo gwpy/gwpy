c86830df9bd304e470dfdb3e6ee8fc82b6a22339
Duncan Macleod <duncan.macleod@ligo.org>
2017-12-15 12:31:19 +0000

    astro.range: fixed type error
    need to operator on `psd` first to preserve output type as `FrequencySeries`

--- gwpy/astro/range.py
+++ gwpy/astro/range.py
@@ -198,7 +198,7 @@ def burst_range_spectrum(psd, snr=8, energy=1e-2):
     # calculate frequency dependent range in parsecs
     a = (constants.G * energy * constants.M_sun * 0.4 /
          (pi**2 * constants.c))**(1/2.)
-    dspec = a / (snr * psd ** (1/2.) * psd.frequencies)
+    dspec = psd ** (-1/2.) * a / (snr * psd.frequencies)
 
     # convert to output unit
     rspec = dspec.to('Mpc')
