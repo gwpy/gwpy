3971f217f7aa2b31e71df26627fd626a2ae60883
Duncan Macleod <duncan.macleod@ligo.org>
2018-02-28 12:11:02 +0000

    examples: fixed some pep8 issues [ci skip]

--- examples/frequencyseries/percentiles.py
+++ examples/frequencyseries/percentiles.py
@@ -39,7 +39,7 @@ __currentmodule__ = 'gwpy.timeseries'
 # First, as always, we get the data using :meth:`TimeSeries.fetch_open_data`:
 
 from gwpy.timeseries import TimeSeries
-hoft = TimeSeries.fetch_open_data('H1', 1187007040, 1187009088, tag='C00', cache=True)
+hoft = TimeSeries.fetch_open_data('H1', 1187007040, 1187009088, tag='C00')
 
 # Next we calculate a :class:`~gwpy.spectrogram.Spectrogram` by calculating
 # a number of ASDs, using the :meth:`~gwpy.timeseries.TimeSeries.spectrogram2`
@@ -64,7 +64,7 @@ ax.plot_spectrum_mmm(median, min_=min_, max_=max_, color='gwpy:ligo-hanford')
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_xlim(10, 1500)
-ax.set_ylim(3e-24,2e-20)
+ax.set_ylim(3e-24, 2e-20)
 ax.set_ylabel(r'Strain noise [1/\rtHz]')
 ax.set_title('LIGO-Hanford strain noise variation around GW170817',
              fontsize=16)
