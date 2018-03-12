70dd858a2149ee8c92a31a997c99f06977922f77
Alexander Urban <alexander.urban@ligo.org>
2018-03-12 13:35:20 -0700

    Adding an example use case to the docstring for demodulate(). This example looks at fluctuations in the amplitude and phase of Livingston's 60 Hz power line harmonic.

--- gwpy/timeseries/timeseries.py
+++ gwpy/timeseries/timeseries.py
@@ -1318,6 +1318,28 @@ class TimeSeries(TimeSeriesBase):
         out : `TimeSeries`
             if `exp=True`, returns a single `TimeSeries` with magnitude and
             phase trends represented as `mag * exp(1j*phase)` with `dt=stride`
+
+        Examples
+        --------
+        Demodulation is useful when trying to examine steady sinusoidal
+        fluctuations we know are contained within some data. For instance,
+        we can download some data from LOSC to look for fluctuations
+        in the 60 Hz power line harmonic:
+
+        >>> from gwpy.timeseries import TimeSeries
+        >>> data = TimeSeries.fetch_open_data('L1', 1131350417, 1131357617)
+        
+        To look at fluctuations we can demodulate the `TimeSeries` at 60 Hz
+        with a stride of once per minute:
+
+        >>> amp, phase = data.demodulate(60, stride=60)
+        
+        We can then plot these trends to visualize changes in the amplitude
+        and phase of the 60 Hz line:
+
+        >>> from gwpy.plotter import TimeSeriesPlot
+        >>> plot = TimeSeriesPlot(amp, phase, sep=True)
+        >>> plot.show()
         """
         stridesamp = int(stride * self.sample_rate.value)
         nsteps = int(self.size // stridesamp)
