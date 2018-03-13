ef841478b77aa76276d859c2e60bd0f1c2a83d92
Alexander Urban <alexander.urban@ligo.org>
2018-03-12 20:58:27 -0700

    Changing the example use case to L1's cal line at 331.3 Hz -- it just works better.

--- gwpy/timeseries/timeseries.py
+++ gwpy/timeseries/timeseries.py
@@ -1323,19 +1323,19 @@ class TimeSeries(TimeSeriesBase):
         --------
         Demodulation is useful when trying to examine steady sinusoidal
         signals we know to be contained within data. For instance,
-        we can download some data from LOSC to look for fluctuations
-        in the 60 Hz power line harmonic:
+        we can download some data from LOSC to look at trends of the
+        amplitude and phase of Livingston's calibration line at 331.3 Hz:
 
         >>> from gwpy.timeseries import TimeSeries
         >>> data = TimeSeries.fetch_open_data('L1', 1131350417, 1131357617)
 
-        We can demodulate the `TimeSeries` at 60 Hz with a stride of once
+        We can demodulate the `TimeSeries` at 331.3 Hz with a stride of once
         per minute:
 
-        >>> amp, phase = data.demodulate(60, stride=60)
+        >>> amp, phase = data.demodulate(331.3, stride=60)
 
         We can then plot these trends to visualize changes in the amplitude
-        and phase of the 60 Hz line:
+        and phase of the calibration line:
 
         >>> from gwpy.plotter import TimeSeriesPlot
         >>> plot = TimeSeriesPlot(amp, phase, sep=True)
