e4439e54c877f9ecc29db330d7eb503c96ae48ff
Alexander Urban <alexander.urban@ligo.org>
2018-04-12 16:05:09 -0700

    Moving FD injection example to examples/frequencyseries

---
+++ examples/frequencyseries/inject.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+
+# Copyright (C) Duncan Macleod (2013)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Inject a known signal into a `FrequencySeries`
+
+It can often be useful to add some known signal to an inherently random
+or noisy timeseries. For example, one might want to examine what
+would happen if a binary black hole merger signal occured at or near
+the time of a glitch. In LIGO data analysis, this procedure is referred
+to as an _injection_.
+
+In the example below, we will create a stream of random, white Gaussian
+noise, then inject a loud, steady sinuosoid.
+"""
+
+__author__ = "Alex Urban <alexander.urban@ligo.org>"
+__currentmodule__ = 'gwpy.timeseries'
+
+# First, we prepare one second of Gaussian noise:
+
+from numpy import random
+from gwpy.timeseries import TimeSeries
+noise = TimeSeries(random.normal(scale=.1, size=1024), sample_rate=1024)
+
+# To inject a signal in the frequency domain, we need to take an FFT:
+
+noisefd = noise.fft()
+
+# We can now easily inject a loud sinusoid at, say, 30 Hz:
+
+import numpy
+from gwpy.frequencyseries import FrequencySeries
+signal = FrequencySeries(numpy.array([1.]), f0=30, df=noisefd.df)
+injfd = noisefd.inject(signal)
+
+# We can then visualize the injection in the frequency domain:
+
+from gwpy.plotter import FrequencySeriesPlot
+plot = FrequencySeriesPlot(numpy.abs(noisefd), numpy.abs(injfd), sep=True,
+                           sharex=True, sharey=True)
+plot.show()
+
+# Finally, for completeness we can visualize the injection in the time domain:
+
+from gwpy.plotter import TimeSeriesPlot
+inj = injfd.ifft()
+plot = TimeSeriesPlot(noise, inj, sep=True, sharex=True, sharey=True)
+plot.show()
+
+# We can also see why sinusoids are easier to inject in the time domain:
+# they only require changing one data point.
