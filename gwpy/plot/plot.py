070d772fc3087cce2ccda1a1c941078297fc7bcc
Duncan Macleod <duncan.macleod@ligo.org>
2018-06-28 04:40:40 -0700

    gwpy.plot: fixed logic for python3

--- gwpy/plot/plot.py
+++ gwpy/plot/plot.py
@@ -516,8 +516,11 @@ def _group_axes_data(inputs, separate=None, flat=False):
         #     the iterable is presumed to be a list of independent data
         #     structures, unless its a list of scalars in which case we
         #     should plot them all as one
-        if isinstance(x, iterable_types) and (
-                not len(x) or not numpy.isscalar(x[0])):
+        if (
+                isinstance(x, (KeysView, ValuesView)) or (
+                isinstance(x, (list, tuple)) and (
+                    not x or not numpy.isscalar(x[0])))
+        ):
             out.append(x)
 
         # dataset starts a new group
