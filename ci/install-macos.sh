ee41df35be00781a7e9f5f78496e32c6d6f69e64
Duncan Macleod <duncan.macleod@ligo.org>
2018-07-04 07:24:05 +0100

    ci: install lscsoft-glue on macos

--- ci/install-macos.sh
+++ ci/install-macos.sh
@@ -76,9 +76,9 @@ sudo port -q install ${PY_PREFIX}-gwpy +nds2 +segments
 # install extras (see requirements-dev.txt)
 sudo port -q install \
     kerberos5 \
-    ${PY_PREFIX}-matplotlib \
     ${PY_PREFIX}-lalsimulation \
     ${PY_PREFIX}-pymysql \
+    ${PY_PREFIX}-lscsoft-glue \
     ${PY_PREFIX}-sqlalchemy \
     ${PY_PREFIX}-psycopg2 \
     ${PY_PREFIX}-pandas \
