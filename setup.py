7c315c95933f0e604c7d21c977c8d033730d6933
Duncan Macleod <duncan.macleod@ligo.org>
2018-07-25 10:23:57 +0100

    added CI job for python3.7
    and classifier in `setup.py`

--- setup.py
+++ setup.py
@@ -146,6 +146,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Intended Audience :: Science/Research',
         'Intended Audience :: End Users/Desktop',
         'Intended Audience :: Developers',
