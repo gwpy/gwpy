2f5e5f3fe26e657e114173eab4d91aa7857ff9f2
Duncan Macleod <duncan.macleod@ligo.org>
2018-08-06 03:08:53 -0700

    gwpy.io: added new gwf.open_gwf function
    modularises opening GWF files with `frameCPP` allowing a simple workaround for errors opening file paths stored as unicode

--- gwpy/timeseries/io/gwf/framecpp.py
+++ gwpy/timeseries/io/gwf/framecpp.py
@@ -106,7 +106,7 @@ def read_gwf(framefile, channels, start=None, end=None, ctype=None,
         end = 0
 
     # open file
-    stream = frameCPP.IFrameFStream(str(framefile))
+    stream = open_gwf(framefile, 'r')
 
     # get number of frames in file
     try:
