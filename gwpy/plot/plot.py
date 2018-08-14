7ebc54260f411efea7bc28f3cb3b2699bd3c33cb
Duncan Macleod <duncan.macleod@ligo.org>
2018-08-14 13:40:53 +0100

    gwpy.plot: refactored duplicate code into loop

--- gwpy/plot/plot.py
+++ gwpy/plot/plot.py
@@ -174,16 +174,17 @@ class Plot(figure.Figure):
             elif group:
                 plot_func(*group, **kwargs)
 
-            # set default axis labels (hide label if shared axis and not
-            #                          bottom left panel)
-            if sharex == 'all' and row < nrows - 1:
-                ax.set_xlabel('')
-            else:
-                ax.xaxis.isDefault_label = defxlabel
-            if sharey == 'all' and col < ncols - 1:
-                ax.set_ylabel('')
-            else:
-                ax.yaxis.isDefault_label = defylabel
+            # set default axis labels
+            for axis, share, pos, n, def_ in (
+                    (ax.xaxis, sharex, row, nrows, defxlabel),
+                    (ax.yaxis, sharey, col, ncols, defylabel),
+            ):
+                # hide label if shared axis and not bottom left panel
+                if share == 'all' and pos < n - 1:
+                    axis.set_label_text('')
+                # otherwise set default status
+                else:
+                    axis.isDefault_label = def_
 
         return self.axes
 
