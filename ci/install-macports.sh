5cb6cb3d3bf5072b6a8dbea1065f2ca410ffb031
Duncan Macleod <duncan.macleod@ligo.org>
2018-10-30 13:55:20 +0000

    ci: refactoring of unix CI jobs

--- ci/install-macports.sh
+++ ci/install-macports.sh
@@ -16,19 +16,31 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
+set -x
+trap 'set +ex' RETURN
+
+function write_visual_bells() {
+    while true; do
+        echo -en "\a"
+        sleep 10
+    done
+}
+
 #
 # Build Port for GWpy
 #
 
-git clone https://github.com/MacPython/terryfy.git
+git clone --quiet --recursive https://github.com/MacPython/terryfy.git
+set +x
 . terryfy/travis_tools.sh
-set -x  # travis_tools.sh sets +x on its way out
+set -ex  # travis_tools.sh sets +x on its way out
 
 export COLUMNS=80  # https://github.com/travis-ci/travis-ci/issues/5407
 install_macports
 
-# get python information
-get_environment
+PYTHON_VERSION=${PYTHON_VERSION:-${TRAVIS_PYTHON_VERSION}}
+PY_DIST="python${PYTHON_VERSION/./}"
+PY_PREFIX="py${PYTHON_VERSION/./}"
 
 # install basic ports we need
 sudo port -q install \
@@ -38,6 +50,7 @@ sudo port -q install \
     ${PY_PREFIX}-pip \
     ${PY_PREFIX}-jinja2 \
     ${PY_PREFIX}-gitpython
+PYTHON=$(which "python${PYTHON_VERSION}")
 
 # make Portfile
 GWPY_VERSION=$($PYTHON setup.py --version)
@@ -45,8 +58,8 @@ $PYTHON setup.py --quiet sdist
 $PYTHON setup.py port --tarball dist/gwpy-${GWPY_VERSION}.tar.gz
 
 # create mock portfile repo and install
-PORT_REPO=$(pwd)/ports
-GWPY_PORT_PATH=${PORT_REPO}/python/py-gwpy
+PORT_REPO="$(pwd)/ports"
+GWPY_PORT_PATH="${PORT_REPO}/python/py-gwpy"
 mkdir -p ${GWPY_PORT_PATH}
 cp Portfile ${GWPY_PORT_PATH}/
 
@@ -54,10 +67,11 @@ cp Portfile ${GWPY_PORT_PATH}/
 gsed -i 's|pypi:g/gwpy|file://'$(pwd)'/dist/ \\\n                    pypi:g/gwpy|' ${GWPY_PORT_PATH}/Portfile
 
 # add local port repo to sources
-sudo gsed -i 's|rsync://rsync.macports|file://'${PORT_REPO}'\nrsync://rsync.macports|' /opt/local/etc/macports/sources.conf
-pushd ${PORT_REPO}
+(
+cd ${PORT_REPO}
+sudo gsed -i 's|rsync://rsync.macports|file://'$(pwd)'\nrsync://rsync.macports|' /opt/local/etc/macports/sources.conf
 portindex
-popd
+)
 
 # set up utility to ping STDOUT every 10 seconds, prevents timeout
 # https://github.com/travis-ci/travis-ci/issues/6591#issuecomment-275804717
@@ -68,33 +82,40 @@ disown
 set -x
 
 # install py-gwpy
-# Note: we don't use the +gwf port, because ldas-tools-framecpp takes too
-#       long to compile that the whole job times out in the end
-sudo port -q install ${PY_PREFIX}-gwpy +nds2 +segments
+sudo port -q install ${PY_PREFIX}-gwpy
 
-# install extras (see requirements-dev.txt)
+# install testing dependencies
 sudo port -q install \
-    kerberos5 \
-    ${PY_PREFIX}-gwdatafind \
-    ${PY_PREFIX}-lalsimulation \
-    ${PY_PREFIX}-pymysql \
-    ${PY_PREFIX}-sqlalchemy \
-    ${PY_PREFIX}-psycopg2 \
-    ${PY_PREFIX}-pandas \
     ${PY_PREFIX}-pytest \
-    ${PY_PREFIX}-coverage \
+    ${PY_PREFIX}-pytest-cov \
     ${PY_PREFIX}-freezegun \
     ${PY_PREFIX}-sqlparse \
     ${PY_PREFIX}-beautifulsoup4
-
-# install python2-only extras
-if [ "${PY_MAJOR_VERSION}" -eq 2 ]; then
-    sudo port -q install glue
+if [[ "${PY_PREFIX}" == "py27" ]]; then
+    sudo port -q install py27-mock
 fi
 
-# install m2cyrpto if needed
-if [ "$(port info --version dqsegdb)" == "version: 1.4.0" ]; then
-    sudo port -q install ${PY_PREFIX}-m2crypto
-fi
+# install extras (see requirements-dev.txt)
+#sudo port -q install \
+#    kerberos5 \
+#    ${PY_PREFIX}-gwdatafind \
+#    ${PY_PREFIX}-lal \
+#    ${PY_PREFIX}-lalframe \
+#    ${PY_PREFIX}-lalsimulation \
+#    ${PY_PREFIX}-nds2-client \
+#    ${PY_PREFIX}-pymysql \
+#    ${PY_PREFIX}-sqlalchemy \
+#    ${PY_PREFIX}-psycopg2 \
+#    ${PY_PREFIX}-pandas
+#
+## install python2.7-only packages
+#if [[ "${PY_PREFIX}" == "py27" ]] then
+#    sudo port -q install dqsegdb
+#    # install m2crypto if needed
+#    if [ "$(port info --version dqsegdb)" == "version: 1.4.0" ]; then
+#        sudo port -q install ${PY_PREFIX}-m2crypto
+#    fi
+#fi
 
+# kill the fancy timeout thing
 kill -9 $wvbpid &> /dev/null
