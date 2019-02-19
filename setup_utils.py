718575bae847844a1dffb620e11ccf792c30d48d
Duncan Macleod <duncan.macleod@ligo.org>
2019-02-19 13:08:09 +0000

    setup_utils.py: added utility to update copyright years

--- setup_utils.py
+++ setup_utils.py
@@ -26,6 +26,7 @@ import datetime
 import glob
 import hashlib
 import os
+import re
 import subprocess
 import shutil
 import sys
@@ -35,6 +36,7 @@ from distutils.command.clean import (clean as orig_clean, log, remove_tree)
 from distutils.command.bdist_rpm import bdist_rpm as distutils_bdist_rpm
 from distutils.errors import DistutilsArgError
 from distutils.version import (LooseVersion, StrictVersion)
+from itertools import groupby
 
 from setuptools.command.bdist_rpm import bdist_rpm as _bdist_rpm
 from setuptools.command.sdist import sdist as _sdist
@@ -45,6 +47,7 @@ CMDCLASS = versioneer.get_cmdclass()
 SETUP_REQUIRES = {
     'test': ['pytest_runner'],
 }
+COPYRIGHT_REGEX = re.compile(r"Copyright[\S ]+(?P<years>\d\d\d\d([, \d-]+)?)")
 
 # -- documentation builder ----------------------------------------------------
 
@@ -516,3 +519,74 @@ def get_scripts(scripts_dir='bin'):
     for (dirname, _, filenames) in os.walk(scripts_dir):
         scripts.extend([os.path.join(dirname, fn) for fn in filenames])
     return scripts
+
+
+def _parse_years(years):
+    """Parse string of ints include ranges into a `list` of `int`
+
+    Source: https://stackoverflow.com/a/6405228/1307974
+    """
+    result = []
+    for part in years.split(','):
+        if '-' in part:
+            a, b = part.split('-')
+            a, b = int(a), int(b)
+            result.extend(range(a, b + 1))
+        else:
+            a = int(part)
+            result.append(a)
+    return result
+
+
+def _format_years(years):
+    """Format a list of ints into a string including ranges
+
+    Source: https://stackoverflow.com/a/9471386/1307974
+    """
+    def sub(x):
+        return x[1] - x[0]
+
+    ranges = []
+    for k, iterable in groupby(enumerate(sorted(years)), sub):
+        rng = list(iterable)
+        if len(rng) == 1:
+            s = str(rng[0][1])
+        else:
+            s = "{}-{}".format(rng[0][1], rng[-1][1])
+        ranges.append(s)
+    return ", ".join(ranges)
+
+
+def update_copyright(path, year):
+    """Update a file's copyright statement to include the given year
+    """
+    with open(path, "r") as fobj:
+        text = fobj.read().rstrip()
+    match = COPYRIGHT_REGEX.search(text)
+    x = match.start("years")
+    y = match.end("years")
+    if text[y-1] == " ":  # don't strip trailing whitespace
+        y -= 1
+    yearstr = match.group("years")
+    years = set(_parse_years(yearstr)) | {year}
+    with open(path, "w") as fobj:
+        print(text[:x] + _format_years(years) + text[y:], file=fobj)
+
+
+def update_all_copyright(year):
+    files = subprocess.check_output([
+        "git", "grep", "-l", "-E", r"(\#|\*) Copyright",
+    ]).strip().splitlines()
+    ignore = {
+        "gwpy/utils/sphinx/epydoc.py",
+        "docs/_static/js/copybutton.js",
+    }
+    for path in files:
+        if path.decode() in ignore:
+            continue
+        try:
+            update_copyright(path, year)
+        except AttributeError:
+            raise RuntimeError(
+                "failed to update copyright for {!r}".format(path),
+            )
