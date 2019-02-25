83031459468209a2c3dc88bacfdf24b4c0e681da
Duncan Macleod <duncan.macleod@ligo.org>
2019-02-25 11:37:11 +0000

    ci: fixed conda update command

--- ci/install-conda.sh
+++ ci/install-conda.sh
@@ -45,7 +45,7 @@ hash -r
 # update conda
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
-conda update --quiet --update-all
+conda update --quiet conda
 conda info --all
 
 # create environment for tests
