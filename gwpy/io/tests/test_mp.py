2ef59b8feff4517c5ba58aa1e3f1de2f0d6f4159
Duncan Macleod <duncan.macleod@ligo.org>
2019-05-23 18:26:29 +0100

    gwpy.io: reworked tests for windows again
    we can't use a proper temporaryfile, since windows doesn't let us open it twice, so we need to use a temporaryfilename and then manually open and write the temporary content into it

--- gwpy/io/tests/test_mp.py
+++ gwpy/io/tests/test_mp.py
@@ -19,14 +19,12 @@
 """Unit tests for :mod:`gwpy.io.mp`
 """
 
-from tempfile import NamedTemporaryFile
-
 import pytest
 
 from astropy import __version__ as astropy_version
 from astropy.table import (Table, vstack)
 
-from ...testing.utils import assert_table_equal
+from ...testing.utils import (assert_table_equal, TemporaryFilename)
 from .. import (
     mp as io_mp,
 )
@@ -36,24 +34,24 @@ __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 
 @pytest.fixture
 def tmp1():
-    with NamedTemporaryFile(suffix='.csv') as tmp:
-        tmp.write(b"a,b,c\n1,2,3\n4,5,6")
-        tmp.seek(0)
+    with TemporaryFilename(suffix='.csv') as tmp:
+        with open(tmp, "w") as tmpf:
+            tmpf.write("a,b,c\n1,2,3\n4,5,6")
         yield tmp
 
 
 @pytest.fixture
 def tmp2():
-    with NamedTemporaryFile(suffix='.csv') as tmp:
-        tmp.write(b"a,b,c\n7,8,9\n10,11,12")
-        tmp.seek(0)
+    with TemporaryFilename(suffix='.csv') as tmp:
+        with open(tmp, "w") as tmpf:
+            tmpf.write("a,b,c\n7,8,9\n10,11,12")
         yield tmp
 
 
 def test_read_multi_single(tmp1):
     """Check that serial processing works with `mp.read_multi`
     """
-    tab = io_mp.read_multi(vstack, Table, tmp1.name, verbose=True)
+    tab = io_mp.read_multi(vstack, Table, tmp1, verbose=True)
     assert tab.colnames == ["a", "b", "c"]
     assert list(tab[0]) == [1, 2, 3]
 
@@ -62,15 +60,15 @@ def test_read_multi_list_of_one(tmp1):
     """Check that a list of one is the same as just passing the element
     """
     assert_table_equal(
-        io_mp.read_multi(vstack, Table, tmp1.name),
-        io_mp.read_multi(vstack, Table, [tmp1.name]),
+        io_mp.read_multi(vstack, Table, tmp1),
+        io_mp.read_multi(vstack, Table, [tmp1]),
     )
 
 
 def test_read_multi_nproc(tmp1, tmp2):
     """Check that simple multiprocessing works with `mp.read_multi`
     """
-    tab = io_mp.read_multi(vstack, Table, [tmp1.name, tmp2.name], nproc=2)
+    tab = io_mp.read_multi(vstack, Table, [tmp1, tmp2], nproc=2)
     assert tab.colnames == ["a", "b", "c"]
     assert list(tab["a"]) == [1, 4, 7, 10]
 
@@ -78,7 +76,7 @@ def test_read_multi_nproc(tmp1, tmp2):
 def test_read_multi_order_preservation(tmp1, tmp2):
     """Check that input order is preserved in `mp.read_multi`
     """
-    tab = io_mp.read_multi(vstack, Table, [tmp2.name, tmp1.name], nproc=2)
+    tab = io_mp.read_multi(vstack, Table, [tmp2, tmp1], nproc=2)
     assert tab.colnames == ["a", "b", "c"]
     assert list(tab["a"]) == [7, 10, 1, 4]
 
@@ -108,9 +106,9 @@ def test_read_multi_error_propagation(tmp1, tmp2, nproc):
     """Check that errors get raised in-place during reads
     """
     # write nonsense into the file
-    tmp1.write(b"blahblahblah\n1,2,3,4blah")
-    tmp1.seek(0)
+    with open(tmp1, "a") as tmp:
+        tmp.write("blahblahblah\n1,2,3,4blah")
     # try and read it
     with pytest.raises(ValueError):
-        io_mp.read_multi(vstack, Table, [tmp1.name, tmp2.name],
+        io_mp.read_multi(vstack, Table, [tmp1, tmp2],
                          format="ascii.csv", nproc=nproc)
