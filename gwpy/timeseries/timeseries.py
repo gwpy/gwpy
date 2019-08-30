b0c9ddcd0c9d2c706352ae9051d75cbd24a1d862
Matthew Pitkin <matthew.pitkin@ligo.org>
2019-08-30 10:18:19 +0100

    heterodyne method documentation change
    Co-Authored-By: Alex L. Urban <aurban1@lsu.edu>

--- gwpy/timeseries/timeseries.py
+++ gwpy/timeseries/timeseries.py
@@ -1442,60 +1442,49 @@ class TimeSeries(TimeSeriesBase):
 
         Examples
         --------
-        Heterodyning can be useful in examining quasi-monochromatic signals
-        with a known phase evolution. Examples of such signals are the
-        `continuous-wave hardware injections
-        <https://www.gw-openscience.org/s6hwcw/>`_ in many of the LIGO
-        observing runs, which are designed to mimic the expected signals
-        from rapidly rotating neutron stars. For these sources the signal's
-        frequency slowly decreases and is also Doppler modulated due to
-        the Earth's rotational and orbital motion.
+        Heterodyning can be useful in analysing quasi-monochromatic signals
+        with a known phase evolution, such as continuous-wave signals
+        from rapidly rotating neutron stars. These sources radiate at a
+        frequency that slowly decreases over time, and is Doppler modulated
+        due to the Earth's rotational and orbital motion.
 
-        We can show an example of demodulation by creating a simulated signal
-        with a phase evolution described by a second order Taylor expansion in
-        phase, i.e., it is defined by a frequency and first frequency
-        derivative. We can download some LIGO Livingston data from GWOSC and
-        inject such a signal and attempt to recover the amplitude.
+        To see an example of heterodyning in action, we can simulate a signal
+        whose phase evolution is described by the frequency and its first
+        derivative with respect to time. We can download some O1 era
+        LIGO-Livingston data from GWOSC, inject the simulated signal, and
+        recover its amplitude.
 
-        >>> from gwpy.timeseries import TimeSeries, TimeSeriesDict
-        >>> data = TimeSeries.fetch_open_data('L1', 1131350417, 1131357617)
+        >>> from gwpy.timeseries import TimeSeries
+        >>> data = TimeSeries.fetch_open_data('L1', 1131350417, 1131354017)
 
         We now need to set the signal parameters, generate the expected
         phase evolution, and create the signal:
 
+        >>> import numpy
         >>> f0 = 123.456789  # signal frequency (Hz)
         >>> fdot = -9.87654321e-7  # signal frequency derivative (Hz/s)
         >>> fpeoch = 1131350417  # phase epoch
         >>> amp = 1.5e-22  # signal amplitude
         >>> phase0 = 0.4  # signal phase at the phase epoch
-        >>> import numpy
         >>> times = data.times.value - fepoch
         >>> phase = 2 * numpy.pi * (f0 * times + 0.5 * fdot * times**2)
         >>> signal = TimeSeries(amp * numpy.cos(phase + phase0),
         >>>                     sample_rate=data.sample_rate, t0=data.t0)
         >>> data = data.inject(signal)
 
-        We now bandpass the data around the signal frequency, and then
-        heterodyne the `TimeSeries` using this phase evolution with a stride of
-        one minute:
+        To recover the signal, we can bandpass the injected data around the
+        signal frequency, then heterodyne using our phase model with a stride
+        of 60 seconds:
 
         >>> filtdata = data.bandpass(f0 - 0.5, f0 + 0.5)
         >>> het = filtdata.heterodyne(phase, stride=60, singlesided=True)
 
-        Let's also demodulate the signal at a fixed freqeuncy for comparison
-
-        >>> demod = filtdata.demodulate(f0, stride=60, exp=True)
-
         We can then plot signal amplitude over time (cropping the first two
         minutes to remove the filter response):
 
-        >>> combined = TimeSeriesDict()
-        >>> combined['Heterodyned'] = het.crop(het.x0.value + 180).abs()
-        >>> combined['Demodulated'] = demod.crop(demod.x0.value + 180).abs()
-        >>> plot = combined.plot()
+        >>> plot = het.crop(het.x0.value + 180).abs().plot()
         >>> ax = plot.gca()
-        >>> ax.legend()
-        >>> ax.set_ylabel("Signal Strain Amplitude")
+        >>> ax.set_ylabel("Strain amplitude")
         >>> plot.show()
 
         See also
