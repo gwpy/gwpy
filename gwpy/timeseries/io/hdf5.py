d9bf51a6b09389ddb59187800fd2129c501382c5
Duncan Macleod <duncan.macleod@ligo.org>
2019-10-02 01:55:06 -0700

    gwpy.timeseries: simplified nested logic in io.hdf5
    Co-authored-by: Alex Urban <aurban1@lsu.edu>

--- gwpy/timeseries/io/hdf5.py
+++ gwpy/timeseries/io/hdf5.py
@@ -41,11 +41,11 @@ def read_hdf5_timeseries(h5f, path=None, start=None, end=None, **kwargs):
     kwargs.setdefault('array_type', TimeSeries)
     series = read_hdf5_array(h5f, path=path, **kwargs)
     # crop if needed
+    if start is not None:
+        start = max(start, series.span[0])
+    if end is not None:
+        end = min(end, series.span[1])
     if start is not None or end is not None:
-        if start is not None:
-            start = max(start, series.span[0])
-        if end is not None:
-            end = min(end, series.span[1])
         return series.crop(start, end)
     return series
 
