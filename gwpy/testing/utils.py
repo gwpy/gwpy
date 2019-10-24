a9076733d85d6a6cf17cdd52033c2143102aeec1
Duncan Macleod <duncan.macleod@ligo.org>
2019-10-24 11:24:00 +0100

    gwpy.testing: compare Time using gps
    this is less susceptible to precision mismatches

--- gwpy/testing/utils.py
+++ gwpy/testing/utils.py
@@ -33,6 +33,8 @@ import pytest
 import numpy
 from numpy.testing import (assert_array_equal, assert_allclose)
 
+from astropy.time import Time
+
 # -- useful constants ---------------------------------------------------------
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
@@ -141,6 +143,8 @@ def assert_attributes(a, b, *attrs):
         y = getattr(b, attr, None)
         if isinstance(x, numpy.ndarray) and isinstance(b, numpy.ndarray):
             assert_array_equal(x, y)
+        elif isinstance(x, Time) and isinstance(y, Time):
+            assert x.gps == y.gps
         else:
             assert x == y
 
