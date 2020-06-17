b4475fd228d5601cba22e49ea5f8cdde12d83cfd
rngeorge <rng5034@gmail.com>
2020-06-17 00:42:04 -0500

    Update examples/table/scatter.py
    Co-authored-by: Duncan Macleod <duncanmmacleod+github@gmail.com>

--- examples/table/scatter.py
+++ examples/table/scatter.py
@@ -34,8 +34,10 @@ from gwpy.table import EventTable
 events = EventTable.fetch_open_data(
     "GWTC-1-confident",
     columns=(
-        "mass_1_source", "mass_2_source",
-        "chirp_mass_source", "luminosity_distance"
+        "mass_1_source",
+        "mass_2_source",
+        "chirp_mass_source",
+        "luminosity_distance"
     ),
 )
 
