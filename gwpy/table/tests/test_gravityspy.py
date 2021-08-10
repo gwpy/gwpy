88fcaf20e5063f6f6eda3ae05cec105a4d04987b
Duncan Macleod <duncan.macleod@ligo.org>
2021-08-10 15:00:35 +0100

    gwpy.table: improve gravityspy tests
    add a test of the `download()` method, and simplify the assertions for the `search()` test

--- gwpy/table/tests/test_gravityspy.py
+++ gwpy/table/tests/test_gravityspy.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) Duncan Macleod (2014-2020)
+# Copyright (C) Louisiana State University (2014-2017)
+#               Cardiff University (2017-2021)
 #
 # This file is part of GWpy.
 #
@@ -16,45 +17,90 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Unit tests for `gwpy.table`
+"""Unit tests for `gwpy.table.gravityspy`
 """
 
-from ...testing import utils
+import pytest
+
+import requests
+
 from ...testing.errors import pytest_skip_network_error
-from .. import GravitySpyTable
+from .. import (
+    gravityspy as table_gravityspy,
+    GravitySpyTable,
+)
 from .test_table import TestEventTable as _TestEventTable
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 
-JSON_RESPONSE = {
-    "url4": [u"https://panoptes-uploads.zooniverse.org/production/"
-             "subject_location/08895951-ea30-4cf7-9374-135a335afe0e.png"],
-    "peak_frequency": [84.4759674072266],
-    "links_subjects": [5740011.0],
-    "ml_label": [u"Scratchy"],
-    "searchedID": [u"8FHTgA8MEu"],
-    "snr": [8.96664047241211],
-    "gravityspy_id": [u"8FHTgA8MEu"],
-    "searchedzooID": [5740011.0],
-    "ifo": [u"H1"],
-    "url3": [u"https://panoptes-uploads.zooniverse.org/production/"
-             "subject_location/415dde44-3109-434c-b3ad-b722a879c159.png"],
-    "url2": [u"https://panoptes-uploads.zooniverse.org/production/"
-             "subject_location/09ebb6f4-e839-466f-80a1-64d79ac4d934.png"],
-    "url1": [u"https://panoptes-uploads.zooniverse.org/production/"
-             "subject_location/5e89d817-583c-4646-8e6c-9391bb99ad41.png"],
-}
+TEST_ID = "8FHTgA8MEu"
+TEST_IFO = "H1"
 
 
 class TestGravitySpyTable(_TestEventTable):
     TABLE = GravitySpyTable
 
-    @pytest_skip_network_error
-    def test_search(self):
-        table = self.TABLE.search(
-            gravityspy_id="8FHTgA8MEu",
+    @classmethod
+    def search(cls):
+        return cls.TABLE.search(
+            gravityspy_id=TEST_ID,
             howmany=1,
             ifos="H1L1",
             timeout=60,
         )
-        utils.assert_table_equal(table, self.TABLE(JSON_RESPONSE))
+
+    @pytest_skip_network_error
+    def test_search(self):
+        """Test `GravitySpyTable.search`
+        """
+        # run the search
+        table = self.search()
+
+        # validate the result
+        assert len(table) == 1
+        t = table[0]
+        assert t["gravityspy_id"] == TEST_ID
+        assert t["ifo"] == TEST_IFO
+        assert t["ml_label"] == "Scratchy"
+
+    def test_search_error(self):
+        """Test `GravitySpyTable.search` error handling
+
+        Mainly to make sure that an HTTP Error is raised instead
+        of a JSONDecodeError.
+        """
+        requests_mock = pytest.importorskip("requests_mock")
+        with requests_mock.Mocker() as rmock, \
+             pytest.raises(requests.HTTPError) as exc:
+            rmock.get(
+                "{}{}{}/?{}".format(
+                    table_gravityspy.DEFAULT_HOST,
+                    table_gravityspy.SEARCH_PATH,
+                    table_gravityspy.SIMILARITY_SEARCH_PATH,
+                    "&".join((
+                        "howmany=10",
+                        "imageid=abcde",
+                        "era=event_time+BETWEEN+1126400000+AND+1584057618",
+                        "ifo=%27H1%27%2C+%27L1%27",
+                        "database=similarity_index_o3",
+                    )),
+                ),
+                text="<!DOCTYPE html><html></html>",
+                status_code=200,
+                headers={
+                    "Content-Type": "text/html; charset=utf-8",
+                },
+            )
+            self.TABLE.search(gravityspy_id="abcde")
+        assert str(exc.value).endswith("please check the request parameters")
+
+    @pytest_skip_network_error
+    def test_download(self, tmp_path):
+        """Test `GravitySpyTable.download`
+        """
+        table = self.search()
+        table.download(download_path=tmp_path)
+        assert (
+            tmp_path
+            / "{}_{}_spectrogram_0.5.png".format(TEST_IFO, TEST_ID)
+        ).is_file()
