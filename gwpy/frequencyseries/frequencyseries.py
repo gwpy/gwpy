044a1707d14de688bf815e4093fb0af49dc29f92
Derek Davis <derek.davis@ligo.org>
2022-08-25 11:15:35 -0700

    Move space in to_pycbc() string to later line

--- gwpy/frequencyseries/frequencyseries.py
+++ gwpy/frequencyseries/frequencyseries.py
@@ -437,8 +437,8 @@ class FrequencySeries(Series):
             epoch = self.epoch.gps
         if self.f0.to('Hz').value:
             raise ValueError(
-                f"Cannot convert FrequencySeries to PyCBC with f0 = {self.f0}. "
-                "Starting frequency must be equal to 0 Hz."
+                f"Cannot convert FrequencySeries to PyCBC with f0 = {self.f0}."
+                " Starting frequency must be equal to 0 Hz."
             )
         return types.FrequencySeries(self.value,
                                      delta_f=self.df.to('Hz').value,
