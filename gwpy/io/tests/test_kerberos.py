c4cf0c58fa61991728fd969f9802b1ca4b62041d
duncan.macleod <duncan.macleod@ligo.org>
2023-01-09 10:03:27 +0000

    gwpy.io: use python-gssapi for kinit
    so we don't have to resort to subprocess

--- gwpy/io/tests/test_kerberos.py
+++ gwpy/io/tests/test_kerberos.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) Duncan Macleod (2014-2020)
+# Copyright (C) Louisiana State University (2014-2017)
+#               Cardiff University (2017-2023)
 #
 # This file is part of GWpy.
 #
@@ -29,178 +30,155 @@ from .. import kerberos as io_kerberos
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 
-# mocked klist output
-KLIST = b"""Keytab name: FILE:/test.keytab
-KVNO Principal
----- -------------------------------
-   1 albert.einstein@LIGO.ORG
-   2 ronald.drever@LIGO.ORG
-   2 ronald.drever@LIGO.ORG"""
 
-# mock os.environ
-MOCK_ENV = None
+@pytest.fixture(autouse=True)
+def mock_krb5_env():
+    with mock.patch.dict(os.environ):
+        for key in (
+            'KRB5_KTNAME',
+            'KRB5CCNAME',
+        ):
+            os.environ.pop(key, None)
+        yield
 
 
-def setup_module():
-    global MOCK_ENV
-    MOCK_ENV = mock.patch.dict(os.environ, {})
-    MOCK_ENV.start()
-    for key in (
-        'KRB5_KTNAME',
-        'KRB5CCNAME',
+def kerberos_name(name):
+    import gssapi
+    return gssapi.Name(
+        base=name,
+        name_type=gssapi.NameType.kerberos_principal,
+    )
+
+
+@mock.patch.dict("sys.modules", {"gssapi": None})
+def test_kinit_no_gssapi():
+    """Test that we get an augmented error message if ``gssapi`` is missing.
+    """
+    with pytest.raises(
+        ImportError,
+        match="cannot generate kerberos credentials without python-gssapi",
     ):
-        os.environ.pop(key, None)
+        io_kerberos.kinit()
 
 
-def teardown_module():
-    if MOCK_ENV is not None:
-        MOCK_ENV.stop()
-
-
-@mock.patch('subprocess.check_output', return_value=KLIST)
-def test_parse_keytab(check_output):
-    """Test `gwpy.io.kerberos.parse_keytab
+@pytest.mark.requires("gssapi")
+@mock.patch("sys.stdout.isatty", return_value=True)
+@mock.patch("gwpy.io.kerberos.input", return_value="rainer.weiss")
+@mock.patch("getpass.getpass", return_value="test")
+@mock.patch("gssapi.raw.acquire_cred_with_password")
+@mock.patch("gssapi.Credentials")
+def test_kinit_up(creds, acquire, getpass, input_, _, capsys):
+    """Test `gwpy.io.kerberos.kinit` with username and password given.
     """
-    # assert principals get extracted correctly
-    principals = io_kerberos.parse_keytab('test.keytab')
-    assert principals == [('albert.einstein', 'LIGO.ORG', 1),
-                          ('ronald.drever', 'LIGO.ORG', 2)]
-
-    # assert klist fail gets raise appropriately
-    check_output.side_effect = [
-        OSError('something'),
-        subprocess.CalledProcessError(1, 'something else'),
-    ]
-    with pytest.raises(io_kerberos.KerberosError) as exc:
-        io_kerberos.parse_keytab('test.keytab')
-    assert str(exc.value) == "Failed to locate klist, cannot read keytab"
-    with pytest.raises(io_kerberos.KerberosError) as exc:
-        io_kerberos.parse_keytab('test.keytab')
-    assert str(exc.value) == "Cannot read keytab 'test.keytab'"
-
-
-@mock.patch('sys.stdout.isatty', return_value=True)
-@mock.patch('gwpy.io.kerberos.input', return_value='rainer.weiss')
-@mock.patch('getpass.getpass', return_value='test')
-@mock.patch('subprocess.Popen')
-def test_kinit_up(popen, getpass, input_, _, capsys):
-    """Test `gwpy.io.kerberos.kinit` with username and password given
-    """
-    proc = popen.return_value
-    proc.poll.return_value = 0
+    acquire.return_value = mock.MagicMock()
 
     # basic call should prompt for username and password
     io_kerberos.kinit()
     input_.assert_called_with(
-        "Please provide username for the LIGO.ORG kerberos realm: ",
+        "Kerberos principal (user@REALM): ",
     )
     getpass.assert_called_with(
         prompt="Password for rainer.weiss@LIGO.ORG: ",
     )
-    popen.assert_called_with(
-        ['kinit', 'rainer.weiss@LIGO.ORG'],
-        stdin=-1,
-        stdout=-1,
-        env=None,
+
+    # and then use the results in gssapi calls
+    acquire.assert_called_with(
+        name=kerberos_name("rainer.weiss@LIGO.ORG"),
+        password="test".encode("utf-8"),
+        usage="initiate",
     )
-    proc.communicate.aossert_called_with(b'test')
 
 
+@pytest.mark.requires("gssapi")
 @mock.patch('gwpy.io.kerberos.input')
 @mock.patch('getpass.getpass')
-@mock.patch('subprocess.Popen')
-def test_kinit_up_kwargs(popen, getpass, input_):
-    """Test `gwpy.io.kerberos.kinit` with username and password given
+@mock.patch("gssapi.raw.acquire_cred_with_password")
+@mock.patch("gssapi.Credentials")
+def test_kinit_up_kwargs(creds, acquire, getpass, input_):
+    """Test `gwpy.io.kerberos.kinit` with username and password given.
     """
-    proc = popen.return_value
-    proc.poll.return_value = 0
-
     io_kerberos.kinit(
-        username='albert.einstein',
-        password='test',
-        exe='/usr/bin/kinit',
+        username="albert.einstein@EXAMPLE.COM",
+        password="test",
     )
     input_.assert_not_called()
     getpass.assert_not_called()
-    popen.assert_called_with(
-        ['/usr/bin/kinit', 'albert.einstein@LIGO.ORG'],
-        stdin=-1,
-        stdout=-1,
-        env=None,
-    )
-    popen.return_value.communicate.assert_called_with(b'test')
-
-
-@mock.patch('gwpy.io.kerberos.parse_keytab')
-@mock.patch('subprocess.Popen')
-def test_kinit_keytab_dne(popen, parse_keytab):
-    """Test `gwpy.io.kerberos.kinit` with a non-existent keytab
-    """
-    proc = popen.return_value
-    proc.poll.return_value = 0
-
-    # test keytab from environment not found (default) prompts user
-    io_kerberos.kinit(username='test', password='passwd',
-                      exe='/bin/kinit')
-    parse_keytab.assert_not_called()
-    popen.assert_called_with(
-        ['/bin/kinit', 'test@LIGO.ORG'],
-        stdin=-1,
-        stdout=-1,
-        env=None,
+    acquire.assert_called_with(
+        name=kerberos_name("albert.einstein@EXAMPLE.COM"),
+        password="test".encode("utf-8"),
+        usage="initiate",
     )
 
 
-@mock.patch.dict(os.environ, {'KRB5_KTNAME': '/test.keytab'})
-@mock.patch('os.path.isfile', return_value=True)
-@mock.patch(
-    'gwpy.io.kerberos.parse_keytab',
-    return_value=[['rainer.weiss', 'LIGO.ORG']],
-)
-@mock.patch('subprocess.Popen')
-def test_kinit_keytab(popen, *unused_mocks):
-    """Test `gwpy.io.kerberos.kinit` can handle keytabs properly
+@pytest.mark.requires("gssapi")
+@mock.patch("gwpy.io.kerberos._acquire_password")
+def test_kinit_keytab_dne(acquire_passwd, tmp_path):
+    """Test `gwpy.io.kerberos.kinit` with a non-existent keytab.
     """
-    proc = popen.return_value
-    proc.poll.return_value = 0
+    keytab = tmp_path / "keytab"  # does not exist
+
+    # check that missing keytab goes to password auth
+    with pytest.warns(
+        UserWarning,
+        match=rf"{keytab.name} is nonexistent or empty\Z",
+    ):
+        io_kerberos.kinit(
+            username='test',
+            password='passwd',
+            keytab=keytab,
+        )
+    acquire_passwd.assert_called_once_with(
+        kerberos_name("test@LIGO.ORG"),
+        "passwd",
+        ccache=None,
+        lifetime=None,
+    )
+
+
+@pytest.mark.requires("gssapi")
+@mock.patch.dict("os.environ")
+@mock.patch("gssapi.Credentials")
+@mock.patch("gwpy.io.kerberos._keytab_principal")
+def test_kinit_keytab(principal, creds, tmp_path):
+    """Test `gwpy.io.kerberos.kinit` can handle keytabs properly.
+    """
+    principal.return_value = kerberos_name("rainer.weiss@LIGO.ORG")
+
+    keytab = tmp_path / "keytab"
+    keytab.touch()
+    ccache = tmp_path / "ccache"
 
     # test keytab kwarg
-    io_kerberos.kinit(keytab='test.keytab', exe='/bin/kinit')
-    popen.assert_called_with(
-        ['/bin/kinit', '-k', '-t', 'test.keytab', 'rainer.weiss@LIGO.ORG'],
-        stdin=-1,
-        stdout=-1,
-        env=None,
+    io_kerberos.kinit(
+        keytab=keytab,
+        ccache=ccache,
+        lifetime=1000,
+    )
+    creds.assert_called_once_with(
+        name=kerberos_name("rainer.weiss@LIGO.ORG"),
+        store={
+            "client_keytab": str(keytab),
+            "ccache": str(ccache),
+        },
+        usage="initiate",
+        lifetime=1000,
     )
 
     # pass keytab via environment
-    io_kerberos.kinit(exe='/bin/kinit')
-    popen.assert_called_with(
-        ['/bin/kinit', '-k', '-t', '/test.keytab', 'rainer.weiss@LIGO.ORG'],
-        stdin=-1,
-        stdout=-1,
-        env=None,
+    creds.reset()
+    os.environ["KRB5_KTNAME"] = str(keytab)
+    io_kerberos.kinit()
+    creds.assert_called_with(
+        name=kerberos_name("rainer.weiss@LIGO.ORG"),
+        store={
+            "client_keytab": str(keytab),
+        },
+        usage="initiate",
+        lifetime=None,
     )
 
 
-@mock.patch('subprocess.Popen')
-def test_kinit_krb5ccname(popen):
-    """Test `gwpy.io.kerberos.kinit` passes `KRB5CCNAME` to /bin/kinit
-    """
-    # test using krb5ccname (credentials cache)
-    # this will raise error because we haven't patched the poll() method
-    # to return 0, but will test that we get the right error
-    with pytest.raises(subprocess.CalledProcessError):
-        io_kerberos.kinit(username='test', password='test',
-                          krb5ccname='/test_cc.krb5', exe='/bin/kinit')
-    popen.assert_called_with(
-        ['/bin/kinit', 'test@LIGO.ORG'],
-        stdin=-1,
-        stdout=-1,
-        env={'KRB5CCNAME': '/test_cc.krb5'},
-    )
-
-
+@pytest.mark.requires("gssapi")
 def test_kinit_notty():
     """Test `gwpy.io.kerberos.kinit` raises an error in a non-interactive
     session if it needs to prompt for information.
@@ -209,4 +187,78 @@ def test_kinit_notty():
     so we don't have to mock anything!
     """
     with pytest.raises(io_kerberos.KerberosError):
-        io_kerberos.kinit(exe='/bin/kinit')
+        io_kerberos.kinit()
+
+
+@pytest.mark.requires("gssapi")
+@mock.patch("gwpy.io.kerberos._acquire_password")
+def test_kinit_error(_acquire_password):
+    """Test that `gwpy.io.kerberos.kinit` propagates `GSSError`s appropriately.
+    """
+    import gssapi
+    _acquire_password.side_effect = gssapi.exceptions.GSSError(0, 0)
+
+    with pytest.raises(
+        io_kerberos.KerberosError,
+        match=(
+            r"\Afailed to generate Kerberos TGT for test@EXAMPLE.COM\Z"
+        ),
+    ):
+        io_kerberos.kinit(
+            username="test@EXAMPLE.COM",
+            password="test",
+        )
+
+
+# -- deprecated
+
+@pytest.mark.requires("gssapi")
+@mock.patch("gwpy.io.kerberos._acquire_password")
+def test_kinit_krb5ccname(_):
+    """Test that the ``krb5ccname`` keyword emits a deprecation warning.
+    """
+    with pytest.warns(
+        DeprecationWarning,
+        match=(
+            "The `krb5ccname` keyword for gwpy.io.kerberos.kinit was renamed"
+        ),
+    ):
+        io_kerberos.kinit(
+            username="test@EXAMPLE.COM",
+            password="test",
+            krb5ccname="cache",
+        )
+
+
+# mocked klist output
+KLIST = b"""Keytab name: FILE:/test.keytab
+KVNO Principal
+---- -------------------------------
+   1 albert.einstein@LIGO.ORG
+   2 ronald.drever@LIGO.ORG
+   2 ronald.drever@LIGO.ORG"""
+
+
+@mock.patch('subprocess.check_output', return_value=KLIST)
+def test_parse_keytab(check_output):
+    """Test `gwpy.io.kerberos.parse_keytab.
+    """
+    # assert principals get extracted correctly
+    with pytest.deprecated_call():
+        principals = io_kerberos.parse_keytab('test.keytab')
+    assert principals == [('albert.einstein', 'LIGO.ORG', 1),
+                          ('ronald.drever', 'LIGO.ORG', 2)]
+
+    # assert klist fail gets raise appropriately
+    check_output.side_effect = [
+        OSError('something'),
+        subprocess.CalledProcessError(1, 'something else'),
+    ]
+    with pytest.deprecated_call(), \
+         pytest.raises(io_kerberos.KerberosError) as exc:
+        io_kerberos.parse_keytab('test.keytab')
+    assert str(exc.value) == "Failed to locate klist, cannot read keytab"
+    with pytest.deprecated_call(), \
+         pytest.raises(io_kerberos.KerberosError) as exc:
+        io_kerberos.parse_keytab('test.keytab')
+    assert str(exc.value) == "Cannot read keytab 'test.keytab'"
