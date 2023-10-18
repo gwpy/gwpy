73cf6e67294e4eafd48d5adde10df027ac3ce985
duncan.macleod <duncan.macleod@ligo.org>
2023-10-18 11:25:16 +0100

    gwpy.utils: fix sphinx/zenodo API calls
    something was updated in the zenodo API which broke stuff,
    see https://github.com/zenodo/zenodo/issues/2483
    
    (cherry picked from commit ee22185a34e3fb86d1868016821eb9807bebe3ec)

--- gwpy/utils/sphinx/zenodo.py
+++ gwpy/utils/sphinx/zenodo.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) Duncan Macleod (2018-2020)
+# Copyright (C) Cardiff University (2018-2023)
 #
 # This file is part of GWpy.
 #
@@ -17,89 +17,146 @@
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
-import requests
 import sys
 
+import requests
 
-def parse_command_line():
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('id', type=int, help='Zenodo ID for package')
-    parser.add_argument('-u', '--url', default='https://zenodo.org/',
-                        help='%(metavar)s to query')
-    parser.add_argument('-n', '--hits', default=10, type=int,
-                        help='number of versions to display')
-    parser.add_argument('-p', '--tag-prefix', default='v',
-                        help='prefix for git version tags')
-    parser.add_argument('-o', '--output-file', help='output file path')
-
-    if len(sys.argv) == 1:  # print --help message for no arguments
-        parser.print_help()
-        sys.exit(0)
-
-    return parser.parse_args()
+DEFAULT_ZENODO_URL = "https://zenodo.org"
+DEFAULT_HITS = 10
 
 
-def format_citations(zid, url='https://zenodo.org/', hits=10, tag_prefix='v'):
-    """Query and format a citations page from Zenodo entries
+def format_citations(
+    zid,
+    url=DEFAULT_ZENODO_URL,
+    hits=10,
+    tag_prefix="v",
+):
+    """Query and format a citations page from Zenodo entries.
 
     Parameters
     ----------
     zid : `int`, `str`
-        the Zenodo ID of the target record
+        The Zenodo ID (``conceptrecid``) of the parent target record.
 
     url : `str`, optional
-        the base URL of the Zenodo host, defaults to ``https://zenodo.org``
+        The base URL of the Zenodo host, defaults to ``https://zenodo.org``.
 
-    hist : `int`, optional
-        the maximum number of hits to show, default: ``10``
+    hits : `int`, optional
+        The maximum number of results to show, default: ``10``.
 
     tag_prefix : `str`, optional
-        the prefix for git tags. This is removed to generate the section
-        headers in the output RST
+        The prefix for git tags. This is removed to generate the section
+        headers in the output RST.
 
     Returns
     -------
     rst : `str`
-        an RST-formatted string of DOI badges with URLs
+        An RST-formatted string of DOI badges with URLs.
     """
     # query for metadata
-    url = ('{url}/api/records/?'
-           'page=1&'
-           'size={hits}&'
-           'q=conceptrecid:"{id}"&'
-           'sort=-version&'
-           'all_versions=True'.format(id=zid, url=url, hits=hits))
-    resp = requests.get(url)  # make the request
+    apiurl = f"{url.rstrip('/')}/api/records"
+    params = {
+        "q": f"conceptrecid:{zid}",
+        "allversions": True,
+        "sort": "version",
+        "page": 1,
+        "size": int(hits),
+    }
+    resp = requests.get(apiurl, params)  # make the request
     resp.raise_for_status()  # make sure it worked
-    metadata = resp.json()  # parse the response
+    records = resp.json()  # parse the response
 
     lines = []
-    for i, hit in enumerate(metadata['hits']['hits']):
-        version = hit['metadata']['version'][len(tag_prefix):]
-        lines.append('-' * len(version))
-        lines.append(version)
-        lines.append('-' * len(version))
-        lines.append('')
-        lines.append('.. image:: {badge}\n'
-                     '   :target: {doi}'.format(**hit['links']))
-        if i < hits - 1:
-            lines.append('')
+    for i, rec in enumerate(records["hits"]["hits"]):
+        # print RST-format header
+        version = str(rec['metadata']['version'])[len(tag_prefix):]
+        head = "-" * len(version)
+        lines.extend([
+            head,
+            version,
+            head,
+            "",
+        ])
 
-    return '\n'.join(lines)
+        # add DOI badge
+        badge = f"{url}/badge/doi/{rec['doi']}.svg"
+        lines.extend([
+            f".. image:: {badge}",
+            f"    :alt: {rec['title']} Zenodo DOI badge",
+            f"    :target: {rec['doi_url']}",
+        ])
+
+        # add break before next record
+        lines.append("")
+
+    return '\n'.join(lines).strip()
+
+
+# -- command-line usage ---------------
+
+def create_parser():
+    """Create an `argparse.ArgumentParser` for this tool.
+    """
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "id",
+        type=int,
+        help="Zenodo concept ID for package",
+    )
+    parser.add_argument(
+        "-u",
+        "--url",
+        default=DEFAULT_ZENODO_URL,
+        help="Base URL of API to query",
+    )
+    parser.add_argument(
+        "-n",
+        "--hits",
+        default=DEFAULT_HITS,
+        type=int,
+        help="Number of versions to display",
+    )
+    parser.add_argument(
+        "-p",
+        "--tag-prefix",
+        default="v",
+        help="Prefix for version tags",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-file",
+        default="stdout",
+        help="Output file path",
+    )
+    return parser
+
+
+def main(args=None):
+    """Run this tool as a command-line script.
+    """
+    # parse arguments
+    parser = create_parser()
+    opts = parser.parse_args(args=args)
+
+    # generate RST
+    citing = format_citations(
+        opts.id,
+        url=opts.url,
+        hits=opts.hits,
+        tag_prefix=opts.tag_prefix,
+    )
+
+    # print
+    if opts.output_file in {None, "stdout"}:
+        f = sys.stdout
+    else:
+        f = open(opts.output_file, 'w')
+    with f:
+        print(citing, file=f)
 
 
 if __name__ == '__main__':
-    args = parse_command_line()
-
-    if args.output_file:
-        f = open(args.output_file, 'w')
-    else:
-        f = sys.stdout
-
-    citing = format_citations(args.id, url=args.url, hits=args.hits,
-                              tag_prefix=args.tag_prefix)
-
-    with f:
-        print(citing, file=f)
+    sys.exit(main())
