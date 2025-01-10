b7d4deac2e018390a83ae2b47d6a7914952cc00d
Duncan Macleod <macleoddm@cardiff.ac.uk>
2025-01-10 11:39:17 +0000

    gwpy.io: add generic ROOT I/O utilities
    just containers an identifier right now

---
+++ gwpy/io/root.py
@@ -0,0 +1,61 @@
+# Copyright (C) Cardiff University (2025-)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""I/O utilities for ROOT files.
+"""
+
+from __future__ import annotations
+
+from typing import IO
+
+ROOT_SIGNATURE = b"root"
+
+
+def identify_root(
+    origin: str,
+    filepath: str | None,
+    fileobj: IO | None,
+    *args,
+    **kwargs,
+) -> bool:
+    """Identify a filename or file object as ROOT.
+
+    This function is only indented to be used as a registered
+    identifier for Astropy's Unified I/O system.
+    """
+    # try and read file descriptor
+    if fileobj is not None:
+        loc = fileobj.tell()
+        fileobj.seek(0)
+        try:
+            signature = fileobj.read(4)
+        finally:
+            fileobj.seek(loc)
+        return signature == ROOT_SIGNATURE
+    # otherwise guess from file name
+    if filepath is not None:
+        return filepath.endswith(".root")
+    # or finally introspect an object from uproot
+    try:
+        import uproot
+    except ImportError:
+        return False
+    else:
+        return isinstance(args[0], (
+            uproot.ReadOnlyFile,
+            uproot.ReadOnlyDirectory,
+        ))
---
+++ gwpy/io/tests/test_root.py
@@ -0,0 +1,70 @@
+# Copyright (C) Cardiff University (2025-)
+#
+# This file is part of GWpy.
+#
+# GWpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for `gwpy.io.root`.
+"""
+
+import pytest
+
+from .. import root as io_root
+
+uproot = pytest.importorskip("uproot")
+
+
+@pytest.fixture
+def rootfile(tmp_path):
+    """Create an empty ROOT file using `uproot.create`.
+    """
+    import uproot
+    path = tmp_path / "test.root"
+    with uproot.create(path):
+        pass
+    return path
+
+
+def test_identify_root_fileobj(rootfile):
+    """Test that `gwpy.io.root.identify_root` works with file objects.
+    """
+    with open(rootfile, "rb") as file:
+        assert io_root.identify_root(
+            "read",
+            None,
+            file,
+        )
+
+
+def test_identify_root_filepath(rootfile):
+    """Test that `gwpy.io.root.identify_root` works with file paths.
+    """
+    assert io_root.identify_root(
+        "read",
+        str(rootfile),
+        None,
+    )
+
+
+def test_identify_root_uproot(rootfile):
+    """Test that `gwpy.io.root.identify_root` works with `uproot` objects.
+    """
+    import uproot
+    with uproot.open(rootfile) as rootf:
+        assert io_root.identify_root(
+            "read",
+            None,
+            None,
+            rootf,
+        )
