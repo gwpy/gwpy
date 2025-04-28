42578f3958ef10df0ca3c844519ee015bce126ac
Duncan Macleod <macleoddm@cardiff.ac.uk>
2025-04-28 12:27:06 +0100

    gwpy.plot: refactor and add type annotations to log.py and tests

--- gwpy/plot/log.py
+++ gwpy/plot/log.py
@@ -1,5 +1,5 @@
-# Copyright (C) Louisiana State University (2014-2017)
-#               Cardiff University (2017-2021)
+# Copyright (c) 2017-2025 Cardiff University
+#               2014-2017 Louisiana State University
 #
 # This file is part of GWpy.
 #
@@ -16,32 +16,58 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
-"""This module redefines the matplotlib log scale to give a more
-helpful set of major and minor ticks.
-"""
+"""Utilities for plotting on logarithmically-scaled axes."""
 
-from math import (ceil, floor, isclose, log)
+from __future__ import annotations
+
+import typing
+from functools import wraps
+from math import (
+    ceil,
+    floor,
+    isclose,
+    log,
+)
 
 import numpy
+from matplotlib import (
+    rcParams,
+    ticker as mticker,
+)
+from matplotlib.scale import (
+    LogScale as _LogScale,
+    register_scale,
+)
 
-from matplotlib import (rcParams, ticker as mticker)
-from matplotlib.scale import (LogScale as _LogScale, register_scale)
+if typing.TYPE_CHECKING:
+    from matplotlib.axis import Axis
+    from numpy.typing import NDArray
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 
 
-def _math(s):
+def _math(s: str | float) -> str:
+    """Format a number using mathtext or TeX."""
     if rcParams["text.usetex"]:
         return f"${s}$"
     return fr"$\mathdefault{{{s}}}$"
 
 
-def _render_simple(values, ndec=2, thresh=100001):
+def _render_simple(
+    values: NDArray,
+    ndec: int = 2,
+    thresh: float = 100001,
+) -> bool:
+    """Return `True` if all of ``values`` can be rendered as simple floats."""
     return (
-        values.size  # is not empty, and
-        and not all(values == 0)  # not all zeros, and
-        and values.max() < thresh  # max below very large number, and
-        and numpy.array_equal(values, values.round(ndec))  # all 2 dec. places
+        # is not empty
+        bool(values.size)
+        # and not all zeros
+        and not all(values == 0)
+        # and max below very large number
+        and values.max() < thresh
+        # and all <2 dec. places
+        and numpy.array_equal(values, values.round(ndec))
     )
 
 
@@ -53,7 +79,10 @@ class LogFormatter(mticker.LogFormatterMathtext):
     range [0.01, 1000) normally, and all others via the standard
     `~matplotlib.ticker.LogFormatterMathtext` output.
     """
-    def format_ticks(self, values):
+
+    @wraps(mticker.LogFormatterMathtext.format_ticks)
+    def format_ticks(self, values: list[float]) -> list[str]:
+        """Format the set of ticks."""
         # this method overrides the default to enable formatting ticks
         # using simple float/integer representations (as opposed) to
         # scientific notation, if _all_ of the ticks have a value
@@ -67,13 +96,16 @@ class LogFormatter(mticker.LogFormatterMathtext):
         # if can render using just "%s" do it
         if _render_simple(values2):
             return [
-                self(int(x) if x.is_integer() else x, pos=i, fmt="%s") for
-                i, x in enumerate(values2)
+                self(int(x) if x.is_integer() else x, pos=i, fmt="%s")
+                for i, x in enumerate(values2)
             ]
         # otherwise use the matplotlib default
         return super().format_ticks(values)
 
-    def _num_ticks(self):
+    def _num_ticks(self) -> int:
+        """Return the number of ticks that should be displayed."""
+        if self.axis is None:
+            return 0
         viewlim = self.axis.get_view_interval()
         loglim = numpy.log10(viewlim)
         return numpy.arange(
@@ -82,7 +114,9 @@ class LogFormatter(mticker.LogFormatterMathtext):
             dtype=int,
         ).size
 
-    def set_locs(self, locs=None):
+    @wraps(mticker.LogFormatterMathtext.set_locs)
+    def set_locs(self, locs: list[float] | None = None) -> None:
+        """Set the locations of the ticks."""
         ret = super().set_locs(locs=locs)
 
         # if a single major tick, but matplotlib decided not to include
@@ -91,29 +125,30 @@ class LogFormatter(mticker.LogFormatterMathtext):
         if (
                 self._num_ticks() == 1
                 and self._sublabels == {1}
-                and self._base == 10
+                and self._base == 10  # noqa: PLR2004
         ):
             self._sublabels = {1., 5., 10.}
 
         return ret
 
-    def __call__(self, x, pos=None, fmt=None):
-        if not x:
+    @wraps(mticker.LogFormatterMathtext.__call__)
+    def __call__(
+        self,
+        x: float,
+        pos: int | None = None,
+        fmt: str | None = None,
+    ) -> str:
+        """Format a tick label."""
+        if x == 0:
             return _math((fmt or "%s") % 0)
-
-        # determine whether to label or not
-        sign = "-" if x < 0 else ""
-        x = abs(x)
-        b = self._base
-        fx = log(x) / log(b)
-
-        is_x_decade = isclose(fx, round(fx))
-        if self.labelOnlyBase and not is_x_decade:
-            return ""
 
         # work out whether to show this label
         # if there are enough major ticks or this formatter doesn't support
         # minor ticks, return a blank string
+        x = abs(x)
+        b = self._base
+        fx = log(x) / log(b)
+        is_x_decade = isclose(fx, round(fx))
         exponent = numpy.round(fx) if is_x_decade else numpy.floor(fx)
         coeff = numpy.round(x / b ** exponent)
         nticks = self._num_ticks()
@@ -126,6 +161,7 @@ class LogFormatter(mticker.LogFormatterMathtext):
 
         # enable custom format
         if fmt:
+            sign = "-" if x < 0 else ""
             return _math(sign + fmt % x)
 
         return super().__call__(x, pos=pos)
@@ -137,12 +173,21 @@ class LogScale(_LogScale):
     This scale overrides the default to use the new GWpy formatters
     for major and minor ticks.
     """
-    def set_default_locators_and_formatters(self, axis):
-        axis.set_major_locator(mticker.LogLocator(self.base))
-        axis.set_major_formatter(LogFormatter(self.base))
-        axis.set_minor_locator(mticker.LogLocator(self.base, self.subs))
+
+    @wraps(_LogScale.set_default_locators_and_formatters)
+    def set_default_locators_and_formatters(self, axis: Axis) -> None:
+        """Set the default locators and formatters."""
+        axis.set_major_locator(
+            mticker.LogLocator(self.base),
+        )
+        axis.set_major_formatter(
+            LogFormatter(self.base),
+        )
+        axis.set_minor_locator(
+            mticker.LogLocator(self.base, self.subs),
+        )
         axis.set_minor_formatter(
-            LogFormatter(self.base, labelOnlyBase=(self.subs is not None)),
+            LogFormatter(self.base, labelOnlyBase=bool(self.subs is not None)),
         )
 
 
