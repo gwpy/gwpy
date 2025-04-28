870fe4ef93e95fcd26bdade82cca8857bc39437a
Duncan Macleod <macleoddm@cardiff.ac.uk>
2025-04-28 13:04:59 +0100

    gwpy.plot: refactor and add type annotations to colors.py and tests

--- gwpy/plot/colors.py
+++ gwpy/plot/colors.py
@@ -1,5 +1,5 @@
-# Copyright (C) Louisiana State University (2017)
-#               Cardiff University (2017-2021)
+# Copyright (c) 2017-2025 Cardiff University
+#               2017 Louisiana State University
 #
 # This file is part of GWpy.
 #
@@ -18,14 +18,31 @@
 
 """Colour customisations for visualisation in GWpy."""
 
-import numpy
-
-from matplotlib import colors
-from matplotlib.colors import (_colors_full_map as color_map, to_rgb)
+from __future__ import annotations
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 
-# -- recommended defaults for current Gravitational-Wave Observatories --------
+import typing
+
+import numpy
+from matplotlib.colors import (
+    LogNorm,
+    Normalize,
+    get_named_colors_mapping,
+    hsv_to_rgb,
+    rgb_to_hsv,
+    to_rgb,
+)
+
+if typing.TYPE_CHECKING:
+    from typing import Any
+
+    from matplotlib.typing import (
+        ColorType,
+        RGBColorType,
+    )
+
+# -- recommended defaults for current Gravitational-Wave Observatories
 # the below colours are designed to work well for the colour-blind, as well
 # as in grayscale, so are recommended for publications
 
@@ -50,33 +67,70 @@ _GWO_PREFIX = {
 GW_OBSERVATORY_COLORS = {_GWO_PREFIX[n]: GWPY_COLORS[n] for n in GWPY_COLORS}
 
 # set named colour upstream in matplotlib, so users can specify as
-# e.g. plot(..., color='gwpy:ligo-hanford')
-color_map.update({f"gwpy:{name}": col for name, col in GWPY_COLORS.items()})
+# e.g. plot(..., color='gwpy:ligo-hanford')           noqa: ERA001
+get_named_colors_mapping().update({
+    f"gwpy:{name}": col for name, col in GWPY_COLORS.items()
+})
 
 
 # -- colour utilities ---------------------------------------------------------
 
-def tint(col, factor=1.0):
-    """Tint a color (make it darker), returning a new RGB array."""
+def tint(col: ColorType, factor: float = 1.0) -> RGBColorType:
+    """Tint a color (make it darker), returning a new RGB array.
+
+    Parameters
+    ----------
+    col : `matplotlib.typing.ColorType`
+        The colour to darken.
+
+    factor : `float`, optional
+        The amount by which to darken it. Values less than ``1`` will make things
+        darker (``0`` will return black), while less than ``1`` will make things
+        lighter.
+
+    Returns
+    -------
+    tinted : `matplotlib.typing.RGBColorType`
+        The new RGB colour tuple.
+    """
     # this method is more complicated than it need be to
     # support matplotlib-1.x.
     # for matplotlib-2.x this would just be
-    #     h, s, v = colors.rgb_to_hsv(colors.to_rgb(c))
-    #     v *= factor
-    #     return colors.hsv_to_rgb((h, s, v))
+    #     h, s, v = rgb_to_hsv(to_rgb(c))  noqa: ERA001
+    #     v *= factor                      noqa: ERA001
+    #     return hsv_to_rgb((h, s, v))     noqa: ERA001
     rgb = numpy.array(to_rgb(col), ndmin=3)
-    hsv = colors.rgb_to_hsv(rgb)
+    hsv = rgb_to_hsv(rgb)
     hsv[-1][-1][2] *= factor
-    return colors.hsv_to_rgb(hsv)[-1][-1]
+    return hsv_to_rgb(hsv)[-1][-1]
 
 
-def format_norm(kwargs, current=None):
+def format_norm(
+    kwargs: dict[str, Any],
+    current: str | Normalize | None = None,
+) -> tuple[Normalize, dict[str, Any]]:
     """Format a `~matplotlib.colors.Normalize` from a set of kwargs.
+
+    Parameters
+    ----------
+    kwargs : `dict`
+        A set of keyword arguments to a plotting routine.
+
+    current : `Normalize`, optional
+        The current normalization, which will be preserved if
+        ``norm`` is not specified in ``kwargs``.
 
     Returns
     -------
     norm, kwargs
-        the formatted `Normalize` instance, and the remaining keywords
+        The formatted `Normalize` instance, and the remaining keywords.
+
+    Raises
+    ------
+    ValueError
+        If ``"norm"`` is included in ``kwargs`` and doesn't match a
+        recognised value (one of ``"linear"``, ``"log"`` or a
+        `~matplotlib.colors.Normalize` instance).
     """
     norm = kwargs.pop("norm", current) or "linear"
     vmin = kwargs.pop("vmin", None)
@@ -85,13 +139,18 @@ def format_norm(kwargs, current=None):
     clip = kwargs.pop("clip", None)
 
     if norm == "linear":
-        norm = colors.Normalize()
+        norm = Normalize()
     elif norm == "log":
-        norm = colors.LogNorm()
-    elif not isinstance(norm, colors.Normalize):
-        raise ValueError(f"unrecognised value for norm '{norm}'")
+        norm = LogNorm()
+    elif not isinstance(norm, Normalize):
+        msg = f"unrecognised value for norm '{norm}'"
+        raise ValueError(msg)
 
-    for attr, value in (("vmin", clim[0]), ("vmax", clim[1]), ("clip", clip)):
+    for attr, value in (
+        ("vmin", clim[0]),
+        ("vmax", clim[1]),
+        ("clip", clip),
+    ):
         if value is not None:
             setattr(norm, attr, value)
 
--- gwpy/plot/tests/test_colors.py
+++ gwpy/plot/tests/test_colors.py
@@ -1,4 +1,4 @@
-# Copyright (C) Duncan Macleod (2018-2020)
+# Copyright (c) 2018-2025 Cardiff University
 #
 # This file is part of GWpy.
 #
@@ -18,44 +18,79 @@
 """Tests for `gwpy.plot.colors`."""
 
 import pytest
-
+from matplotlib.colors import (
+    LogNorm,
+    Normalize,
+)
 from numpy.testing import assert_array_equal
-
-from matplotlib.colors import (Normalize, LogNorm)
 
 from .. import colors as plot_colors
 
 
-@pytest.mark.parametrize("in_, factor, out", [
-    ("red", 1., (1., 0., 0.)),
-    ((1., 0., 0.), 1., (1., 0., 0.)),
-    ("green", .75, (0.0, 0.37647058823529411, 0.0)),
+@pytest.mark.parametrize(("in_", "factor", "out"), [
+    pytest.param(
+        "red",
+        1.,
+        (1., 0., 0.),
+        id="red",
+    ),
+    pytest.param(
+        (1., 0., 0.),
+        1.,
+        (1., 0., 0.),
+        id="red-rgb",
+    ),
+    pytest.param(
+        "green",
+        .75,
+        (0.0, 0.37647058823529411, 0.0),
+        id="green",
+    ),
 ])
 def test_tint(in_, factor, out):
-    assert_array_equal(plot_colors.tint(in_, factor=factor), out)
+    """Test `tint()`."""
+    assert_array_equal(
+        plot_colors.tint(in_, factor=factor),
+        out,
+    )
 
 
 def test_format_norm():
-    # defaults
-    norm, kwargs = plot_colors.format_norm({})
+    """Test `format_norm()`."""
+    norm, kwargs = plot_colors.format_norm({"blah": 1})
     assert isinstance(norm, Normalize)
-    assert kwargs == {}
+    assert kwargs == {"blah": 1}
 
-    # log norm
-    norm, kwargs = plot_colors.format_norm(
-        {"norm": "log", "vmin": 1, "vmax": 10})
+
+def test_format_norm_log():
+    """Test `format_norm()` handling of ``norm="log"``."""
+    norm, kwargs = plot_colors.format_norm({
+        "norm": "log",
+        "vmin": 1,
+        "vmax": 10,
+    })
     assert isinstance(norm, LogNorm)
     assert norm.vmin == 1
     assert norm.vmax == 10
+    assert not kwargs
 
-    # existing norm, change limits
+
+def test_format_norm_instance():
+    """Test `format_norm()` handling of ``norm=<Normalize>``."""
     n = LogNorm()
-    norm, kwargs = plot_colors.format_norm(
-        {"norm": n, "clim": (10, 1000)})
+    norm, kwargs = plot_colors.format_norm({
+        "norm": n,
+        "clim": (10, 1000),
+    })
     assert norm is n
     assert norm.vmin == 10
     assert norm.vmax == 1000
+    assert not kwargs
 
-    # check clim=None is honoured
+
+def test_format_norm_clim():
+    """Test `format_norm()` handling of ``clim``."""
     norm, kwargs = plot_colors.format_norm({"clim": None})
-    assert norm.vmin is None and norm.vmax is None
+    assert norm.vmin is None
+    assert norm.vmax is None
+    assert not kwargs
