35ac13714ae22a93f41c896938a2fe01c869e62c
Duncan Macleod <macleoddm@cardiff.ac.uk>
2025-05-29 17:30:02 +0100

    gwpy.types: reformat index.py and tests with type annotations

--- gwpy/types/tests/test_index.py
+++ gwpy/types/tests/test_index.py
@@ -1,4 +1,5 @@
-# Copyright (C) Duncan Macleod (2014-2020)
+# Copyright (c) Cardiff University (2017-2025)
+#               Louisiana State University (2014-2017)
 #
 # This file is part of GWpy.
 #
@@ -15,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Unit tests for :mod:`gwpy.types.index`."""
+"""Tests for :mod:`gwpy.types.index`."""
 
 from astropy import units
 
@@ -23,6 +24,8 @@ from .. import Index
 
 
 class TestIndex:
+    """Test `gwpy.types.Index`."""
+
     TEST_CLASS = Index
 
     def test_define_regular(self):
@@ -31,7 +34,7 @@ class TestIndex:
         a = self.TEST_CLASS.define(
             1262936373.4853957,
             0.051,
-            ind_len
+            ind_len,
         )
         assert len(a) == ind_len
 
@@ -45,21 +48,37 @@ class TestIndex:
         assert a.is_regular()
 
     def test_is_regular(self):
+        """Test `Index.is_regular()`."""
         a = self.TEST_CLASS([1, 2, 3, 4, 5, 6], "s")
         assert a.is_regular()
         assert a[::-1].is_regular()
 
+    def test_is_regular_scalar(self):
+        """Test `Index.is_regular()` returns `False` for scalars."""
+        a = self.TEST_CLASS([1])
+        assert not a.is_regular()
+
     def test_not_is_regular(self):
+        """Test `Index.is_regular()` (return `False`)."""
         b = self.TEST_CLASS([1, 2, 4, 5, 7, 8, 9])
         assert not b.is_regular()
 
     def test_regular(self):
+        """Test `Index.regular`."""
         a = self.TEST_CLASS([1, 2, 3, 4, 5, 6], "s")
         assert a.regular
         assert a.regular is a.info.meta["regular"]
 
     def test_getitem(self):
+        """Test `Index.__getitem__()`."""
         a = self.TEST_CLASS([1, 2, 3, 4, 5, 6], "Hz")
         assert type(a[0]) is units.Quantity
         assert a[0] == 1 * units.Hz
         assert isinstance(a[:2], type(a))
+
+    def test_setitem(self):
+        """Test `Index.__setitem__()`."""
+        a = self.TEST_CLASS([1, 2, 3, 4, 5, 6], "Hz")
+        a[0:2] = [7, 8] * a.unit
+        assert list(a.value) == [7, 8, 3, 4, 5, 6]
+        assert a[0] == 7 * units.Hz
